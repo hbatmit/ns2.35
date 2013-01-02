a6fcc308bcd8e76a3db55fa1423610a73efedb2c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-01-02 16:20:39 -0500

    Ensure delays are greater than 0.

--- queue/qdelay-estimator.cc
+++ queue/qdelay-estimator.cc
@@ -14,7 +14,7 @@ std::vector<uint64_t> QdelayEstimator::estimate_delays( uint64_t current_tick )
 	/* Base case(W_0) in Lindley's recurrence */
 	Packet hol_pkt = _pkts.front();
 	_pkts.pop();
-	uint64_t current_delay = current_tick - hol_pkt._tick + _service_time;
+	int64_t current_delay = current_tick - hol_pkt._tick + _service_time;
 	_delays.push_back( current_delay );
 	Packet previous_pkt = hol_pkt;
 
@@ -22,8 +22,10 @@ std::vector<uint64_t> QdelayEstimator::estimate_delays( uint64_t current_tick )
 	while ( !_pkts.empty() ) {
 		Packet current_pkt = _pkts.front();
 		_pkts.pop();
-		uint64_t inter_arrival_time = current_pkt._tick - previous_pkt._tick;
-		current_delay = std::max( current_delay + _service_time - inter_arrival_time, (uint64_t) 0 );
+		int64_t inter_arrival_time = current_pkt._tick - previous_pkt._tick;
+		assert( inter_arrival_time >= 0 );
+		current_delay = std::max( (int64_t) (current_delay + _service_time - inter_arrival_time), (int64_t) 0 );
+		assert( current_delay >= 0 );
 		_delays.push_back( current_delay );
 		}
 
