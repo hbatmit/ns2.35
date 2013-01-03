5418322c441cebeea044ab09e2d2161449527cf3
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-01-03 11:38:09 -0500

    Modified estimator and link to unit test Lindley's recurrence, test seems to pass.

--- queue/qdelay-estimator.cc
+++ queue/qdelay-estimator.cc
@@ -15,9 +15,10 @@ std::vector<uint64_t> QdelayEstimator::estimate_delays( uint64_t current_tick )
 	/* Base case(W_0) in Lindley's recurrence */
 	Packet hol_pkt = _pkts.front();
 	_pkts.pop();
-	int64_t current_delay = current_tick - hol_pkt._tick + _service_time;
+	int64_t current_delay = current_tick - hol_pkt._tick + 0;
 	_delays.push_back( current_delay );
 	Packet previous_pkt = hol_pkt;
+	fprintf( stderr, "Lindley : seqnum %lu delay %ld\n", hol_pkt._seq_num, current_delay);
 
 	/* Apply Lindley's recurrence using constant service time */
 	while ( !_pkts.empty() ) {
@@ -28,6 +29,7 @@ std::vector<uint64_t> QdelayEstimator::estimate_delays( uint64_t current_tick )
 		current_delay = std::max( (int64_t) (current_delay + _service_time - inter_arrival_time), (int64_t) 0 );
 		assert( current_delay >= 0 );
 		_delays.push_back( current_delay );
+		fprintf( stderr, "Lindley : seqnum %lu delay %ld\n", current_pkt._seq_num, current_delay);
 		previous_pkt = current_pkt ;
 		}
 
