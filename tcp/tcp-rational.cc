6278af12a2758a6ddd1ab8e1faef0b18d03660ed
Keith Winstein <keithw@mit.edu>
2013-01-26 18:55:50 -0500

    Print more complete memory and whisker info

--- tcp/tcp-rational.cc
+++ tcp/tcp-rational.cc
@@ -224,8 +224,8 @@ RationalTcpAgent::update_cwnd_and_pacing( void )
 	cwnd_ = new_cwnd;
 	_intersend_time = .001 * current_whisker.intersend();
 	if (tracewhisk_) {
-		fprintf( stderr, "whisker: %s\n", _memory.str().c_str() );
-		fprintf( stderr, "\tcwnd now %u, intersend_time now %f\n", new_cwnd, _intersend_time );
+		fprintf( stderr, "memory: %s falls into whisker %s\n", _memory.str().c_str(), current_whisker.str().c_str() );
+		fprintf( stderr, "\t=> cwnd now %u, intersend_time now %f\n", new_cwnd, _intersend_time );
 	}
 
 	//	fprintf( stderr, "cwnd now %u, intersend_time now %f\n", new_cwnd, _intersend_time );
