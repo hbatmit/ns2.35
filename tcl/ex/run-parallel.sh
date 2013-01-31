f316745ead7a5e111a08ce41ec53746efed4ac51
Keith Winstein <keithw@mit.edu>
2013-01-31 01:12:20 -0500

    Use flowcdf

--- tcl/ex/run-parallel.sh
+++ tcl/ex/run-parallel.sh
@@ -4,10 +4,10 @@ export WHISKERS=home/hari/ns2.35/tcp/remy/rats/rat1.dna.35
 
 cd /home/hari/ns2.35/tcl/ex
 
-python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t bytes -p TCP/Newreno &
-python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t bytes -p TCP/Linux/cubic &
-python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t bytes -p TCP/Linux/compound &
-python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t bytes -p TCP/Vegas &
-python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t bytes -p TCP/Reno/XCP &
-python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t bytes -p TCP/Newreno/Rational &
-python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t bytes -p Cubic/sfqCoDel &
+python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Newreno &
+python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Linux/cubic &
+python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Linux/compound &
+python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Vegas &
+python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Reno/XCP &
+python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Newreno/Rational &
+python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p Cubic/sfqCoDel &
