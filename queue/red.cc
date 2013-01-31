61305f678ad6072c4745d15811aee24cfa91321c
Hari Balakrishnan <hari@csail.mit.edu>
2013-01-31 15:20:04 -0500

    DCTCP from Stanford: http://www.stanford.edu/~alizade/Site/DCTCP.html

--- queue/red.cc
+++ queue/red.cc
@@ -559,7 +559,7 @@ REDQueue::drop_early(Packet* pkt)
 		edv_.count_bytes = 0;
 		hdr_flags* hf = hdr_flags::access(pickPacketForECN(pkt));
 		if (edp_.setbit && hf->ect() && 
-                     (!edp_.use_mark_p || edv_.v_prob1 < edp_.mark_p)) { 
+		    (!edp_.use_mark_p || edv_.v_prob1 <= edp_.mark_p)) { // For DCTCP: '<' is changed to '<=' here  
 			hf->ce() = 1; 	// mark Congestion Experienced bit
 			// Tell the queue monitor here - call emark(pkt)
 			return (0);	// no drop
