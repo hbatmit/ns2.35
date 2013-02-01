350da28f89681f1086604bd741435ba542e724b3
Keith Winstein <keithw@mit.edu>
2013-01-31 19:35:50 -0500

    Updates to parallel runner

--- tcl/ex/run-parallel.sh
+++ tcl/ex/run-parallel.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/expand100x-delta256.out
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p TCP/Newreno &
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p TCP/Linux/cubic &
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p TCP/Linux/compound &
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p TCP/Vegas &
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p TCP/Reno/XCP &
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p Cubic/sfqCoDel &
 
-cd tcl/ex
+export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta0.1.dna
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf-rational-delta0.1 -t flowcdf -p TCP/Rational &
 
-#python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Newreno &
-#python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Linux/cubic &
-#python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Linux/compound &
-#python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Vegas &
-#python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p TCP/Reno/XCP &
-python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf-superexpand -t flowcdf -p TCP/Rational &
-#python runremy.py -c remyconf/vz4gdown.tcl -d Verizon-flowcdf -t flowcdf -p Cubic/sfqCoDel &
+export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta0.5.dna
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf-rational-delta0.5 -t flowcdf -p TCP/Rational &
+
+export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta1.dna
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf-rational-delta1 -t flowcdf -p TCP/Rational &
+
+export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta2.dna
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf-rational-delta2 -t flowcdf -p TCP/Rational &
+
+export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta10.dna
+python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf-rational-delta10 -t flowcdf -p TCP/Rational &
