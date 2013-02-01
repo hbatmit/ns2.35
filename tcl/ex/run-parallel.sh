2981bb02968816f3669fd61b46a3679acfbd771a
Keith Winstein <keithw@tom.csail.mit.edu>
2013-01-31 21:20:24 -0500

    Perl parallel

--- tcl/ex/run-parallel.sh
+++ tcl/ex/run-parallel.sh
@@ -1,23 +1,25 @@
-#!/bin/sh
+#!/usr/bin/perl -w
 
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p TCP/Newreno &
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p TCP/Linux/cubic &
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p TCP/Linux/compound &
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p TCP/Vegas &
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p TCP/Reno/XCP &
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf -t flowcdf -p Cubic/sfqCoDel &
+for my $nsrc ( 1, 2, 4, 8, 16, 32 ) {
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential -t bytes -p TCP/Newreno -n $nsrc &");
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential -t bytes -p TCP/Linux/cubic -n $nsrc &");
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential -t bytes -p TCP/Linux/compound -n $nsrc &");
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential -t bytes -p TCP/Vegas -n $nsrc &");
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential -t bytes -p TCP/Reno/XCP -n $nsrc &");
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential -t bytes -p Cubic/sfqCoDel -n $nsrc &");
 
-export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta0.1.dna
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf-rational-delta0.1 -t flowcdf -p TCP/Rational &
+    $ENV{'WHISKERS'} = '/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta0.1.dna';
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential-rational-delta0.1 -t bytes -p TCP/Rational -n $nsrc &");
 
-export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta0.5.dna
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf-rational-delta0.5 -t flowcdf -p TCP/Rational &
+    $ENV{'WHISKERS'} = '/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta0.5.dna';
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential-rational-delta0.5 -t bytes -p TCP/Rational -n $nsrc &");
 
-export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta1.dna
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf-rational-delta1 -t flowcdf -p TCP/Rational &
+    $ENV{'WHISKERS'} = '/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta1.dna';
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential-rational-delta1 -t bytes -p TCP/Rational -n $nsrc &");
 
-export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta2.dna
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf-rational-delta2 -t flowcdf -p TCP/Rational &
+    $ENV{'WHISKERS'} = '/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta2.dna';
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential-rational-delta2 -t bytes -p TCP/Rational -n $nsrc &");
 
-export WHISKERS=/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta10.dna
-python runremy.py -c remyconf/equisource.tcl -d Ethernet-flowcdf-rational-delta10 -t flowcdf -p TCP/Rational &
+    $ENV{'WHISKERS'} = '/home/keithw/gradschool/ns2.35/tcp/remy/rats/new/delta10.dna';
+    system("python runremy.py -c remyconf/equisource.tcl -d Ethernet-Exponential-rational-delta10 -t bytes -p TCP/Rational -n $nsrc &");
+}
