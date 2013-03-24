7f246e0caac4e2459b89692566651b42c5f87f71
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-03-23 22:44:35 -0400

    Add packet type for Cellular link layer.

--- common/packet.h
+++ common/packet.h
@@ -198,9 +198,10 @@ static const packet_t PT_DCCP_RESET = 71;
 
         // M-DART packets
 static const packet_t PT_MDART = 72;
-	
+
+static const packet_t PT_CELLULAR = 73;
         // insert new packet types here
-static packet_t       PT_NTYPE = 73; // This MUST be the LAST one
+static packet_t       PT_NTYPE = 74; // This MUST be the LAST one
 
 enum packetClass
 {
@@ -417,6 +418,7 @@ public:
 		name_[PT_DCCP_CLOSEREQ]="DCCP_CloseReq";
 		name_[PT_DCCP_RESET]="DCCP_Reset";
 
+                name_[PT_CELLULAR]="CellularLink";
 		name_[PT_NTYPE]= "undefined";
 	}
 	static int addPacket(char *name);
