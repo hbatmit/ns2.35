4790e7ec334ece2cecdd78e287d53acb7188c97e
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-03-23 22:45:46 -0400

    Add PT_CELLULAR to ns-packet.tcl as well.

--- tcl/lib/ns-packet.tcl
+++ tcl/lib/ns-packet.tcl
@@ -183,6 +183,7 @@ set protolist {
 	Encap 	# common/encap.cc
         IPinIP 	# IP encapsulation 
 	HDLC 	# High Level Data Link Control
+        CELLULAR # Cellular networks
 }
 set allhdrs [regsub -all {#.*?\n} $protolist \n]; # strip comments from above
 foreach prot $allhdrs {
