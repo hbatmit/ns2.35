4d14b75dd9f8ac445a23ed29f2c5130b0d6bf785
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-03-24 12:24:47 -0400

    Add helper functions to slice packet and fill in various fields.

--- link/cell_header.cc
+++ link/cell_header.cc
@@ -9,3 +9,49 @@ static class CellularHeaderClass : public PacketHeaderClass {
      bind_offset(&hdr_cellular::offset_);
   }
 } class_cellularhdr;
+
+void hdr_cellular::fill_in(Packet* p,
+                           int slice_size,
+                           bool last_fragment,
+                           packet_t tunneled_type,
+                           int original_size) {
+  /* Fill in hdr_cmn fields */
+  /* Shorten the packet so that txtime() works correctly */
+  hdr_cmn::access(p)->size() = slice_size;
+
+  /* Change the packet type so that Classifier::recv() works */
+  hdr_cmn::access(p)->ptype() = PT_CELLULAR;
+
+  /* Now fill in hdr_cellular fields required for reassembly */
+  /* Is this the last fragment ? For classifier::recv() */
+  hdr_cellular::access(p)->last_fragment_ = last_fragment;
+
+  /* What is the tunneled type ? For classifier::recv() */
+  hdr_cellular::access(p)->tunneled_type_ = tunneled_type;
+
+  /* What was the original packet size ? For classifier::recv() */
+  hdr_cellular::access(p)->original_size_ = original_size;
+}
+
+Packet* hdr_cellular::slice(Packet *p, int slice_bytes) {
+  /* Slice packet */
+  Packet* sliced_pkt = p->copy();
+
+  /* Find tunneled_type and original_size */
+  packet_t tunneled_type;
+  int original_size;
+  if (hdr_cmn::access(p)->ptype() == PT_CELLULAR) { 
+    /* It's already been split up, so look "recursively" */
+    tunneled_type = hdr_cellular::access(p)->tunneled_type_;
+    original_size = hdr_cellular::access(p)->original_size_;
+
+  } else {
+    /* Look at the hdr_common fields */
+    tunneled_type = hdr_cmn::access(p)->ptype();
+    original_size = hdr_cmn::access(p)->size();
+  }
+
+  /* Fill in */
+  hdr_cellular::fill_in(sliced_pkt, slice_bytes, false, tunneled_type, original_size);
+  return sliced_pkt;
+}
--- link/cell_header.h
+++ link/cell_header.h
@@ -6,12 +6,22 @@
 struct hdr_cellular {
   bool last_fragment_;
   packet_t tunneled_type_;
+  int original_size_;
 
   /* Packet header access functions */
   static int offset_;
   inline static int& offset() { return offset_; }
   inline static hdr_cellular* access(const Packet* p) { return (hdr_cellular*)p->access(offset_);}
 
+  /* Fill in fields */
+  static void fill_in(Packet* p,
+                      int slice_size,
+                      bool last_fragment,
+                      packet_t tunneled_type,
+                      int original_size);
+
+  /* Slice packet */
+  static Packet* slice(Packet *p, int slice_bytes);
 };
 
 #endif
