50863ef6f8f79a3cfe68827828061702056ee57c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-03-25 14:48:13 -0400

    Considerably simplify sfd. Still need to fix a TODO, but compiles.

--- queue/sfd.cc
+++ queue/sfd.cc
@@ -19,21 +19,17 @@ int SFD::command(int argc, const char*const* argv)
 
 SFD::SFD() :
   LinkAwareQueue(),
-  _packet_queues( std::map<uint64_t,PacketQueue*>() ),
-  _dropper( SfdDropper( &_packet_queues ) ),
-  _timestamps( std::map<uint64_t,std::queue<uint64_t>> () ),
-  _counter( 0 ),
-  _scheduler( &_packet_queues , &_timestamps ),
+  _packet_queue( new PacketQueue() ),
+  _dropper(),
   _rate_estimator( 0.0, 0.0 )
 {
   bind("_iter", &_iter );
   bind("_qdisc", &_qdisc );
   bind("_K", &_K );
   bind("_headroom", &_headroom );
-  fprintf( stderr,  "SFD: _iter %d, _qdisc %d , _K %f, _headroom %f \n", _iter, _qdisc, _K, _headroom );
+  bind("user_id", &user_id);
+  fprintf( stderr,  "SFD: _iter %d, _qdisc %d , _K %f, _headroom %f, user_id %d \n", _iter, _qdisc, _K, _headroom, user_id );
   _dropper.set_iter( _iter );
-  _scheduler.set_iter( _iter );
-  _scheduler.set_qdisc( _qdisc );
   _rate_estimator = SfdRateEstimator( _K, _headroom );
 }
 
@@ -41,50 +37,33 @@ void SFD::enque(Packet *p)
 {
   /* Implements pure virtual function Queue::enque() */
 
-  /* Compute flow_id using hash */
-  uint64_t flow_id = hash( p );
-
-  /* Estimate arrival rate */
+  /* Estimate arrival rate with an EWMA filter */
   double now = Scheduler::instance().clock();
-  double arrival_rate = _rate_estimator.est_flow_arrival_rate( flow_id, now, p );
+  double arrival_rate = _rate_estimator.est_flow_arrival_rate( user_id, now, p );
 
-  /* Estimate current EWMA link rate for flow. */
-  auto current_link_rates = get_current_link_rates();
-
-  /* Check if flow has been seen earlier */
-  if ( current_link_rates.find( flow_id ) == current_link_rates.end() ) {
-    enque_packet( p, flow_id );
-    return;
-  }
+  /* Estimate current link rate with an EWMA filter. */
+  auto current_link_rate = _rate_estimator.est_flow_link_rate(user_id, now, _link->bandwidth());
 
   /* Divide Avg. link rate by # of active flows to get fair share */
-  auto _fair_share = ( _rate_estimator.est_flow_link_rate( flow_id, now, current_link_rates.at( flow_id ) ) )/_packet_queues.size();
+  auto _fair_share = current_link_rate/5 /* TODO: Fix this */;
 
   /* Print everything */
   print_stats( now );
-
-  /* Extract protocol (TCP vs UDP) from the header */
-  hdr_cmn* hdr  = hdr_cmn::access(p);
-  packet_t pkt_type   = hdr->ptype();
 
   /* Compute drop_probability */
   double drop_probability = std::max( 0.0 , 1 - _fair_share/arrival_rate );
 
   /* Toss a coin and drop */
   if ( !_dropper.should_drop( drop_probability ) ) {
-    printf( " Time %f : Not dropping packet of type %d , from flow %lu drop_probability is %f\n", now, pkt_type, flow_id, drop_probability );
-    enque_packet( p, flow_id );
+    printf( " Time %f : Not dropping packet, from flow %u drop_probability is %f\n", now, user_id, drop_probability );
+    _packet_queue->enque( p );
   } else {
     /* Drop from front of the same queue */
-    uint64_t drop_flow = flow_id ;
-    printf( " Time %f : Dropping packet of type %d, from flow %lu drop_probability is %f\n", now, pkt_type, drop_flow, drop_probability );
-    enque_packet( p, flow_id );
-    Packet* head = _packet_queues.at( drop_flow )->deque();
+    printf( " Time %f : Dropping packet, from flow %u drop_probability is %f\n", now, user_id, drop_probability );
+    _packet_queue->enque(p);
+    Packet* head = _packet_queue->deque();
     if (head != 0 ) {
         drop( head );
-    }
-    if ( !_timestamps.at( drop_flow ).empty() ) {
-      _timestamps.at( drop_flow ).pop();
     }
   }
 }
@@ -92,76 +71,20 @@ void SFD::enque(Packet *p)
 Packet* SFD::deque()
 {
   /* Implements pure virtual function Queue::deque() */
-
   double now = Scheduler::instance().clock();
-  uint64_t current_flow = (uint64_t)-1;
-
-  if ( _qdisc == QDISC_FCFS ) {
-    current_flow = _scheduler.fcfs_scheduler();
-  } else if ( _qdisc == QDISC_RAND ) {
-    current_flow = _scheduler.random_scheduler();
-  } else {
-    assert( false );
-  }
-
-  if ( _packet_queues.find( current_flow ) != _packet_queues.end() ) {
-    if ( !_timestamps.at( current_flow ).empty() ) {
-      _timestamps.at( current_flow ).pop();
-    }
-    Packet *p = _packet_queues.at( current_flow )->deque();
-    _rate_estimator.est_flow_service_rate( current_flow, now, p );
-    print_stats( now );
-    return p;
-  } else {
-    _rate_estimator.est_flow_service_rate( current_flow, now, nullptr );
-    print_stats( now );
-    return 0; /* empty */
-  }
-}
-
-void SFD::enque_packet( Packet* p, uint64_t flow_id )
-{
-  /* Push into flow queue, if it doesn't exist create one.
-     Also push into _timestamps for FCFS */
-
-  if ( _packet_queues.find( flow_id )  != _packet_queues.end() ) {
-    _packet_queues.at( flow_id )->enque( p );
-    _timestamps.at( flow_id ).push( ++_counter );
-  } else {
-    _packet_queues[ flow_id ] = new PacketQueue();
-    _timestamps[ flow_id ] = std::queue<uint64_t>();
-    _packet_queues.at( flow_id )->enque( p );
-    _timestamps.at( flow_id ).push( ++_counter );
-  }
+  Packet *p = _packet_queue->deque();
+  _rate_estimator.est_flow_service_rate( user_id, now, p );
+  print_stats( now );
+  return p;
 }
 
 void SFD::print_stats( double now )
 {
   /* Queue sizes */
   printf(" Time %f : Q :  ", now );
-  std::for_each( _packet_queues.begin(), _packet_queues.end(), [&] ( const std::pair<uint64_t,PacketQueue*> q ) { printf(" %lu %d ", q.first, q.second->length()); } );
+  printf(" %u %d ", user_id, _packet_queue->length());
   printf("\n");
 
   /* Arrival, Service, fair share, and ingress rates */
   _rate_estimator.print_rates( now );
-}
-
-std::map<uint64_t,double> SFD::get_current_link_rates( void ) const
-{
-  auto link_rates = _link->get_current_link_rates();
-  std::map<uint64_t,double> link_speeds;
-
-  for ( auto q_it = _packet_queues.begin(); q_it != _packet_queues.end(); q_it++ ) {
-    link_speeds[ q_it->first ]=link_rates[ q_it->first ];
-  }
-  return link_speeds;
-}
-
-std::vector<uint64_t> SFD::backlogged_flowids( void ) const
-{
-  std::vector<uint64_t> backlogged_flows;
-  for (auto it=_packet_queues.begin(); it!=_packet_queues.end(); it++) {
-    backlogged_flows.push_back( it->first );
-  }
-  return backlogged_flows;
 }
--- queue/sfd.h
+++ queue/sfd.h
@@ -27,39 +27,27 @@ class SFD : public LinkAwareQueue {
     double  _K;        /* default : 200 ms */
     double  _headroom; /* default : 0.05 */
     int _iter;         /* random seed */
+    int user_id;       /* unique user_id */
 
-    /* Underlying per flow FIFOs and enque wrapper */
-    std::map<uint64_t,PacketQueue*> _packet_queues;
-    void enque_packet( Packet* p, uint64_t flow_id );
+    /* Underlying FIFO */
+    PacketQueue* _packet_queue;
 
     /* Random dropper */
     SfdDropper _dropper;
-
-    /* Scheduler */
-    std::map<uint64_t,std::queue<uint64_t>> _timestamps; /* For FCFS */
-    uint64_t _counter;
-    SfdScheduler _scheduler;
-
-    /* Hash from packet to flow */
-    uint64_t hash( Packet *p ) { return ( hdr_ip::access(p) )->flowid(); };
 
     /* Rate Estimator */
     SfdRateEstimator _rate_estimator;
 
   public :
     SFD();
-    int command(int argc, const char*const* argv);
+    int command(int argc, const char*const* argv) override;
 
     /* print stats  */
     void print_stats( double now );
 
-    /* inherited functions from LinkAwareQueue */
-    std::map<uint64_t,double> get_current_link_rates() const;
-    virtual std::vector<uint64_t> backlogged_flowids( void ) const override;
-
     /* inherited functions from queue */
-    virtual void enque( Packet *p );
-    virtual Packet* deque();
+    virtual void enque( Packet *p ) override;
+    virtual Packet* deque() override;
 };
 
 #endif
