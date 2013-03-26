0daaebd6a0b34ee040268b1742b4248edca9a2f8
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-03-26 16:10:58 -0400

    Code clean up, call nodes client and server correctly.

--- tcl/ex/cell-link.tcl
+++ tcl/ex/cell-link.tcl
@@ -59,53 +59,54 @@ set num_users [ expr $opt(num_tcp) + $opt(num_udp)  ]
 puts "Num users is $num_users, cdma rates available for $opt(cdma_users) users "
 assert ( $num_users <= $opt(cdma_users) );
 
-# Change parameters for both schedulers */
+# Change parameters for both schedulers
 FcfsScheduler set num_users_ $num_users
-PFScheduler set num_users_ $num_users
-PFScheduler set slot_duration_  $opt(cdma_slot_duration)
-PFScheduler set ewma_slots_ $opt(cdma_ewma_slots)
+PFScheduler   set num_users_ $num_users
+PFScheduler   set slot_duration_  $opt(cdma_slot_duration)
+PFScheduler   set ewma_slots_ $opt(cdma_ewma_slots)
 
+# Pick appropriate ensemble_scheduler
 if { $opt(ensemble_scheduler) == "pf" } {
   set ensemble_scheduler [ new PFScheduler ]
 } elseif { $opt(ensemble_scheduler) == "fcfs" } {
   set ensemble_scheduler [ new FcfsScheduler ]
 }
 
-# Set K and headroom for SFD
+# Set _K and _headroom for SFD
 Queue/SFD set _K $opt(_K)
 Queue/SFD set _headroom $opt(headroom)
 
 # Unique ID
 set counter 0
 
-# TCP clients
+# TCP servers
 for { set i 0 } { $i < $opt(num_tcp) } { incr i } {
-  # Create UDP Agents
-  set tcp_client($i) [ new Agent/TCP/Linux ]
-  $tcp_client($i) select_ca cubic
-  $ns attach-agent $basestation $tcp_client($i)
+  # Create TCP Agents
+  set tcp_server($i) [ new Agent/TCP/Linux ]
+  $tcp_server($i) select_ca cubic
+  $ns attach-agent $basestation $tcp_server($i)
 
   # set flow id
-  $tcp_client($i) set fid_ $counter
+  $tcp_server($i) set fid_ $counter
   set fid($i) $counter
   set counter [ incr counter ]
 
   # Generate FTP traffic on TCP Agent
-  set ftp_client($i) [ new Application/FTP ]
-  $ftp_client($i) attach-agent $tcp_client($i)
-  $ns at 0.0 "$ftp_client($i) start"
+  set ftp_server($i) [ new Application/FTP ]
+  $ftp_server($i) attach-agent $tcp_server($i)
+  $ns at 0.0 "$ftp_server($i) start"
 }
 
-# TCP servers
+# TCP clients
 for { set i 0 } { $i < $opt(num_tcp) } { incr i } {
-  # Create node
-  set tcp_server_node($i) [ $ns node ]
-  $ns simplex-link $basestation $tcp_server_node($i) [ bw_parse $opt(bottleneck_bw) ]  $opt(bottleneck_latency) $opt(bottleneck_qdisc)
-  $ns simplex-link $tcp_server_node($i) $basestation [ bw_parse $opt(bottleneck_bw) ]  $opt(bottleneck_latency) DropTail
+  # Create node corresponding to mobile user
+  set tcp_client_node($i) [ $ns node ]
+  $ns simplex-link $basestation $tcp_client_node($i) [ bw_parse $opt(bottleneck_bw) ]  $opt(bottleneck_latency) $opt(bottleneck_qdisc)
+  $ns simplex-link $tcp_client_node($i) $basestation [ bw_parse $opt(bottleneck_bw) ]  $opt(bottleneck_latency) DropTail
 
-  # Attach queue and link to PF scheduler
-  set cell_link [ [ $ns link $basestation $tcp_server_node($i) ] link ]
-  set cell_queue [ [ $ns link $basestation $tcp_server_node($i) ] queue ]
+  # Get handles to link and queue
+  set cell_link [ [ $ns link $basestation $tcp_client_node($i) ] link ]
+  set cell_queue [ [ $ns link $basestation $tcp_client_node($i) ] queue ]
 
   # All the non-standard queue neutering:
   # block queues
@@ -118,7 +119,7 @@ for { set i 0 } { $i < $opt(num_tcp) } { incr i } {
   # Deactivate forward queue
   $cell_queue deactivate_queue
 
-  # Set user id and other stuff for SFD
+  # Set user_id and other stuff for SFD
   if { $opt(bottleneck_qdisc) == "SFD" } {
     $cell_queue user_id $i
     $cell_queue attach-link $cell_link
@@ -126,54 +127,54 @@ for { set i 0 } { $i < $opt(num_tcp) } { incr i } {
   }
 
   # Attach trace_file to queue.
-  $ns trace-queue $basestation $tcp_server_node($i) $trace_file
+  $ns trace-queue $basestation $tcp_client_node($i) $trace_file
 
-  puts "Adding user $fid($i) to PF "
+  # Attach queue and link to ensemble_scheduler
+  puts "Adding user $fid($i) to scheduler"
   $ensemble_scheduler attach-queue $cell_queue $fid($i)
   $ensemble_scheduler attach-link  $cell_link  $fid($i)
 
-  # Crate tcp sinks
-  set tcp_server($i) [ new Agent/TCPSink/Sack1 ]
-  $ns attach-agent $tcp_server_node($i) $tcp_server($i)
+  # Create tcp sinks
+  set tcp_client($i) [ new Agent/TCPSink/Sack1 ]
+  $ns attach-agent $tcp_client_node($i) $tcp_client($i)
 
   # Connect them to their sources
-  $ns connect $tcp_client($i) $tcp_server($i)
+  $ns connect $tcp_server($i) $tcp_client($i)
 }
 
-# CBR/UDP clients
+# CBR/UDP server
 for { set i 0 } { $i < $opt(num_udp) } { incr i } {
   # Create UDP Agents
-  set udp_client($i) [ new Agent/UDP ]
-  $ns attach-agent $basestation $udp_client($i)
+  set udp_server($i) [ new Agent/UDP ]
+  $ns attach-agent $basestation $udp_server($i)
 
   # set flow id
-  $udp_client($i) set fid_ $counter
+  $udp_server($i) set fid_ $counter
   set fid($i) $counter
   set counter [ incr counter ]
 
   # Generate CBR traffic on UDP Agent
-  set cbr_client($i) [ new Application/Traffic/CBR ]
-  $cbr_client($i) attach-agent $udp_client($i)
+  set cbr_server($i) [ new Application/Traffic/CBR ]
+  $cbr_server($i) attach-agent $udp_server($i)
 
   # Set packetSize_ and related parameters.
-  $cbr_client($i) set packetSize_ 1000
+  $cbr_server($i) set packetSize_ 1000
   puts "UDP rate $opt(cbr_rate)"
-  $cbr_client($i) set rate_   [ bw_parse $opt(cbr_rate) ]
-  $cbr_client($i) set random_     0
-  $ns at 0.0 "$cbr_client($i) start"
+  $cbr_server($i) set rate_   [ bw_parse $opt(cbr_rate) ]
+  $cbr_server($i) set random_     0
+  $ns at 0.0 "$cbr_server($i) start"
 }
 
-# CBR/UDP servers
+# CBR/UDP clients
 for { set i 0 } { $i < $opt(num_udp) } { incr i } {
-  # Create node
-  set udp_server_node($i) [ $ns node ]
-  $ns simplex-link $basestation $udp_server_node($i) [ bw_parse $opt(bottleneck_bw) ]  $opt(bottleneck_latency) $opt(bottleneck_qdisc)
-  $ns simplex-link $udp_server_node($i) $basestation [ bw_parse $opt(bottleneck_bw) ]  $opt(bottleneck_latency) DropTail
+  # Create node corresponding to mobile user
+  set udp_client_node($i) [ $ns node ]
+  $ns simplex-link $basestation $udp_client_node($i) [ bw_parse $opt(bottleneck_bw) ]  $opt(bottleneck_latency) $opt(bottleneck_qdisc)
+  $ns simplex-link $udp_client_node($i) $basestation [ bw_parse $opt(bottleneck_bw) ]  $opt(bottleneck_latency) DropTail
 
-
-  # Attach queue and link to PF scheduler
-  set cell_link [ [ $ns link $basestation $udp_server_node($i) ] link ]
-  set cell_queue [ [ $ns link $basestation $udp_server_node($i) ] queue ]
+  # Get references to cell_link and cell_queue
+  set cell_link [ [ $ns link $basestation $udp_client_node($i) ] link ]
+  set cell_queue [ [ $ns link $basestation $udp_client_node($i) ] queue ]
 
   # All the non-standard queue neutering:
   # block queues
@@ -194,18 +195,19 @@ for { set i 0 } { $i < $opt(num_udp) } { incr i } {
   }
 
   # Attach trace_file to queue.
-  $ns trace-queue $basestation $udp_server_node($i) $trace_file
+  $ns trace-queue $basestation $udp_client_node($i) $trace_file
 
-  puts "Adding user $fid($i) to PF "
+  # Attach queue and link to ensemble scheduler
+  puts "Adding user $fid($i) to scheduler "
   $ensemble_scheduler attach-queue $cell_queue $fid($i)
   $ensemble_scheduler attach-link  $cell_link  $fid($i)
 
   # Create Application Sinks
-  set udp_server($i) [ new Agent/Null ]
-  $ns attach-agent $udp_server_node($i) $udp_server($i)
+  set udp_client($i) [ new Agent/Null ]
+  $ns attach-agent $udp_client_node($i) $udp_client($i)
 
   # Connect them to their sources
-  $ns connect $udp_client($i) $udp_server($i)
+  $ns connect $udp_server($i) $udp_client($i)
 }
 
 # Activate scheduler
