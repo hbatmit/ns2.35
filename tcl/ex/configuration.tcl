116aed8cc9af77031792526625a146ff4d39ce27
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-03-31 14:49:53 -0400

    Get rid of unused parameters.

--- tcl/ex/configuration.tcl
+++ tcl/ex/configuration.tcl
@@ -1,9 +1,5 @@
 set opt(num_udp)            1
 set opt(num_tcp)            1
-set opt(ingress_bw)         20Mb
-set opt(ingress_latency)    1ms
-set opt(egress_bw)          20Mb
-set opt(egress_latency)     1ms
 set opt(bottleneck_bw)      10Mb
 set opt(bottleneck_latency) 20ms
 set opt(bottleneck_qdisc)   SFD
@@ -12,12 +8,9 @@ set opt(codel_target)       5ms
 set opt(codel_interval)     100ms
 set opt(cbr_rate)           10Mb
 set opt(iter)               1
-set opt(link_type)          cellular
-set opt(sfd_qdisc)          fcfs
 set opt(_K)                 0.2
 set opt(headroom)           0.05
 set opt(cdma_slot_duration) 0.00167
-set opt(cdma_users)         5
 set opt(ensemble_scheduler) pf
 set opt(cdma_ewma_slots)    100
 set opt(congestion_control) cubic
@@ -44,4 +37,3 @@ Agent/TCP/FullTcp set windowInit_ 4
 Agent/TCP/FullTcp set segsperack_ 1
 Agent/TCP/FullTcp set timestamps_ true
 Agent/TCP/FullTcp set interval_   $opt(delack)
-
