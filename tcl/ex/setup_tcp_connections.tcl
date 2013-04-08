c7a19b9d5a5273cc772a1bc0a3b4a6dd5549cc26
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-04-07 20:46:09 -0400

    Replace infinite FTP stream with ON/OFF TCP Traffic.

--- tcl/ex/setup_tcp_connections.tcl
+++ tcl/ex/setup_tcp_connections.tcl
@@ -1,7 +1,12 @@
+# source other files
+
+source logging_app.tcl
+source stat_collector.tcl
+
 # TCP servers
 for { set i 0 } { $i < $opt(num_tcp) } { incr i } {
   # Create TCP Agents
-  set tcp_server($i) [ new Agent/TCP/Linux ]
+  set tcp_server($i) [ new Agent/$opt(tcp) ]
   $tcp_server($i) select_ca $opt(congestion_control)
   $ns attach-agent $basestation $tcp_server($i)
 
@@ -10,10 +15,8 @@ for { set i 0 } { $i < $opt(num_tcp) } { incr i } {
   set fid($i) $counter
   set counter [ incr counter ]
 
-  # Generate FTP traffic on TCP Agent
-  set ftp_server($i) [ new Application/FTP ]
-  $ftp_server($i) attach-agent $tcp_server($i)
-  $ns at 0.0 "$ftp_server($i) start"
+  # Generate ON/OFF traffic on TCP Agent
+  set src($fid($i)) [ $tcp_server($i) attach-source $opt(app) ]
 }
 
 # TCP clients
@@ -46,7 +49,21 @@ for { set i 0 } { $i < $opt(num_tcp) } { incr i } {
   # Create tcp sinks
   set tcp_client($i) [ new Agent/TCPSink/Sack1 ]
   $ns attach-agent $tcp_client_node($i) $tcp_client($i)
+  set onoff_server($i) [new LoggingApp $fid($i)]
+  $onoff_server($i) attach-agent $tcp_client($i)
+  $ns at 0.0 "$onoff_server($i) start"
+
+  # start only the odd-numbered connections immediately
+  if { [expr $i % 2] == 0 } {
+    $onoff_server($i) go 0.0
+  } else {
+    $onoff_server($i) go [$onoff_server($i) sample_off_duration]
+  }
 
   # Connect them to their sources
   $ns connect $tcp_server($i) $tcp_client($i)
+  set tcp_senders($fid($i)) $tcp_server($i)
+
+  # Setup stat collector
+  set stats($i) [new StatCollector $i $opt(tcp)]
 }
