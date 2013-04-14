6e0264090c3e05d4ce5977836b41014d51902412
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-04-14 10:36:27 -0400

    More tests; SFD vs CoDel on identical dynamic link traces with 2 users.

--- tcl/ex/regression/regression_tests.sh
+++ tcl/ex/regression/regression_tests.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 cd ..
-# SFD test
+
+# SFD test on 100 TCP users on fixed link rates from 1 through 100 mbps
 set -v
 set -x
 ../../ns cell-link.tcl -bottleneck_qdisc SFD -num_tcp 100  -num_udp 0 -duration 100 -ensemble_scheduler fcfs -congestion_control cubic  -headroom 0.0 -link_trace regression/1to100mbps.trace > /dev/null 2> /dev/null
@@ -11,7 +12,7 @@ python ../../utils/parse-trace.py cell-link.tr 100 0.95 > /dev/null 2> test.outp
 diff test.output regression/sfd-tcp100-1to100mbps.output
 echo "Result of test is $?"
 
-# CoDel test
+# CoDel test on 100 TCP users on fixed link rates from 1 through 100 mbps
 set -v
 set -x
 ../../ns cell-link.tcl -bottleneck_qdisc CoDel -num_tcp 100  -num_udp 0 -duration 100 -ensemble_scheduler pf -cdma_ewma_slots 100 -congestion_control cubic -headroom 0.0 -link_trace regression/1to100mbps.trace > /dev/null 2> /dev/null
@@ -39,6 +40,26 @@ set +v
 set +x
 python ../../utils/parse-trace.py cell-link.tr 100 0.95  > /dev/null 2> test.output
 diff test.output regression/codel-tcp1-v4grates.output
+echo "Result of test is $?"
+
+# SFD on 100ms coherence time Verizon 4G trace, with 2 identical users
+set -v
+set -x
+../../ns cell-link.tcl -bottleneck_qdisc SFD -num_tcp 2  -num_udp 0 -duration 100 -ensemble_scheduler fcfs -congestion_control cubic -headroom 0.0 -link_trace regression/v4g.2users > /dev/null 2> /dev/null
+set +v
+set +x
+python ../../utils/parse-trace.py cell-link.tr 100 0.95  > /dev/null 2> test.output
+diff test.output regression/sfd-tcp2-v4grates.2users
+echo "Result of test is $?"
+
+# CoDel on 100ms coherence time Verizon 4G trace, with 2 identical users.
+set -v
+set -x
+../../ns cell-link.tcl -bottleneck_qdisc CoDel -num_tcp 2  -num_udp 0 -duration 100 -ensemble_scheduler pf -cdma_ewma_slots 100 -congestion_control cubic  -headroom 0.0 -link_trace regression/v4g.2users >  /dev/null 2> /dev/null 
+set +v
+set +x
+python ../../utils/parse-trace.py cell-link.tr 100 0.95  > /dev/null 2> test.output
+diff test.output regression/codel-tcp2-v4grates.2users
 echo "Result of test is $?"
 
 cd -
