753766178844b3e6d616bb8b7a3628eec0e48b11
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-04-16 13:50:38 -0400

    Add regression test with ON/OFF TCP pattern.

--- tcl/ex/regression/regression_tests.sh
+++ tcl/ex/regression/regression_tests.sh
@@ -22,6 +22,26 @@ python ../../utils/parse-trace.py cell-link.tr 100 0.95 > /dev/null 2> test.outp
 diff test.output regression/codel-tcp100-1to100mbps.output
 echo "Result of test is $?"
 
+# SFD test on 100 TCP users on fixed link rates from 1 through 100 mbps; TCP ON/OFF Pattern
+set -v
+set -x
+../../ns cell-link.tcl -bottleneck_qdisc SFD -num_tcp 100  -num_udp 0 -duration 100 -ensemble_scheduler fcfs -congestion_control cubic  -headroom 0.0 -link_trace regression/1to100mbps.trace -enable_on_off true  > /dev/null 2> /dev/null
+set +v
+set +x
+python ../../utils/parse-trace.py cell-link.tr 100 0.95 > /dev/null 2> test.output
+diff test.output regression/sfd-tcp100-dyn-1to100mbps.output
+echo "Result of test is $?"
+
+# CoDel test on 100 TCP users on fixed link rates from 1 through 100 mbps; TCP ON/OFF Pattern
+set -v
+set -x
+../../ns cell-link.tcl -bottleneck_qdisc CoDel -num_tcp 100  -num_udp 0 -duration 100 -ensemble_scheduler pf -cdma_ewma_slots 100 -congestion_control cubic  -headroom 0.0 -link_trace regression/1to100mbps.trace -enable_on_off true > /dev/null 2> /dev/null
+set +v
+set +x
+python ../../utils/parse-trace.py cell-link.tr 100 0.95 > /dev/null 2> test.output
+diff test.output regression/codel-tcp100-dyn-1to100mbps.output
+echo "Result of test is $?"
+
 # SFD on 100ms coherence time Verizon 4G trace.
 set -v
 set -x
