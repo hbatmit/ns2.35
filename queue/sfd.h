9a7acac6d38fd0c0357001ce1f5c7c4b43a83ebd
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-04-25 11:01:04 -0400

    Implement get_service_rate() in SFD

--- queue/sfd.h
+++ queue/sfd.h
@@ -49,6 +49,7 @@ class SFD : public EnsembleAwareQueue {
     virtual bool empty() const override { return (_packet_queue->byteLength() == 0); }
     virtual double get_hol() const override { return (empty()) ? DBL_MAX : hdr_cmn::access(_packet_queue->head())->timestamp(); }
     virtual double get_arrival_rate() const override { return _rate_estimator._arr_est.get_rate(); }
+    virtual double get_service_rate() const override { return _rate_estimator._ser_est.get_rate(); }
 
 };
 
