4d94f22fd5436e11c2f5f38955011187306e47c8
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-04-25 10:16:50 -0400

    Better comments. Understood prop. fairness a little better.

--- utils/prop-fair.py
+++ utils/prop-fair.py
@@ -8,8 +8,11 @@ import numpy
 slot=0
 TOTAL_SLOTS=100000
 NUM_USERS=10
+# initial values close to zero.
 T=[0.00001]*NUM_USERS
+# link rates
 R=[120.0,240.0,360.0,480.0,600.0,720.0,840.0,960.0,1080.0,1200.0]
+# EWMA constant
 t_C=20.0;
 
 while( slot < TOTAL_SLOTS ) :
@@ -20,7 +23,7 @@ while( slot < TOTAL_SLOTS ) :
   # Update averages
   for i in range(0,NUM_USERS) :
     if   (i == user) :
-      T[ i ] = (1-1/t_C)*T[ i ] + 1/t_C*R[ i ]
+      T[ i ] = (1-(1/t_C))*T[ i ] + (1/t_C)*R[ i ]
     elif (i != user) :
-      T[ i ] = (1-1/t_C)*T[ i ]
+      T[ i ] = (1-(1/t_C))*T[ i ]
   slot = slot + 1;
