51a68a77130b960b8496e5e302c46e5ef16757e9
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-05-01 17:08:26 -0400

    Make EWMAEstimator more generic and not specific to rates

--- common/ewma-estimator.cc
+++ common/ewma-estimator.cc
@@ -2,32 +2,32 @@
 #include <assert.h>
 #include <math.h>
 
-EwmaEstimator::EwmaEstimator( double K, double initial_rate, double initial_time ) :
-  _rate_estimate( initial_rate ),
+EwmaEstimator::EwmaEstimator( double K, double initial_value, double initial_time ) :
+  _estimate( initial_value ),
   _time_constant( K ),
   _last_update( initial_time )
 {}
 
 EwmaEstimator::EwmaEstimator() :
-  _rate_estimate( -1 ),
+  _estimate( -1 ),
   _time_constant( -1 ),
   _last_update( -1 )
 {}
 
-double EwmaEstimator::update( double now, double current_rate )
+double EwmaEstimator::update( double now, double current_value )
 {
   /* Ensure some time has passed since last update */
   auto inter_update_interval = now - _last_update;
 
   /* Apply EWMA */
   _last_update = now;
-  _rate_estimate =
-    (1.0 - exp(-inter_update_interval/_time_constant))*current_rate +
-    exp(-inter_update_interval/_time_constant)*_rate_estimate;
-  return _rate_estimate;
+  _estimate =
+    (1.0 - exp(-inter_update_interval/_time_constant))*current_value +
+    exp(-inter_update_interval/_time_constant)*_estimate;
+  return _estimate;
 }
 
-double EwmaEstimator::get_rate( void ) const
+double EwmaEstimator::get_estimate( void ) const
 {
-  return _rate_estimate;
+  return _estimate;
 }
