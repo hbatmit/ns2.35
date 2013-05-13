88e00e92d0258d7800b62a68f8e53b7e666fecb0
Hari Balakrishnan <hari@csail.mit.edu>
2013-05-13 14:40:59 -0400

    Congestion control module (MIT / Remy & Onramp projects)

---
+++ tcl/ex/congctrl/logging-app.tcl
@@ -0,0 +1,154 @@
+# Author : Hari Balakrishnan
+# Class that simulates random on/off traffic, where
+# on and off durations are drawn from a specific distribution.
+source timer.tcl
+
+Class LoggingApp -superclass {Application Timer}
+
+LoggingApp instproc init {id} {
+    $self instvar srcid_ nbytes_ cumrtt_ numsamples_ u_ offtotal_ on_ranvar_ off_ranvar_ rtt_samples_ flowfile
+    global opt
+    $self set srcid_ $id
+    $self set nbytes_ 0
+    $self set cumrtt_ 0.0
+    $self set rtt_samples_ {}
+    $self set numsamples_ 0
+    $self set u_ [new RandomVariable/Uniform]
+    $self set offtotal_ 0.0
+    
+    # Internet flow CDF distribution
+    $self set flowfile flowcdf-allman-icsi.tcl
+
+    # Create exponential on/off RandomVariables
+    $self set on_ranvar_  [new RandomVariable/$opt(onrand)]
+    $self set off_ranvar_ [new RandomVariable/$opt(offrand)]
+    if { $opt(ontype) == "time" } {
+        $on_ranvar_ set avg_ $opt(onavg)
+    } elseif { $opt(ontype) == "bytes" } {
+            $on_ranvar_ set avg_ $opt(avgbytes)
+    } elseif { $opt(ontype) == "flowcdf" } {
+        source $flowfile
+    }
+    set off_ranvar_ [new RandomVariable/$opt(offrand)]
+    $off_ranvar_ set avg_ $opt(offavg)
+
+    $self settype
+    $self next
+}
+
+LoggingApp instproc settype { } {
+    $self instvar endtime_ maxbytes_ 
+    global opt
+    if { $opt(ontype) == "time" } {
+        $self set maxbytes_ "infinity"; # not byte-limited
+        $self set endtime_ 0
+    } else {
+        $self set endtime_ $opt(simtime)
+        $self set maxbytes_ 0        
+    }
+}
+
+# called at the start of the simulation for the first run
+LoggingApp instproc go { starttime } {
+    $self instvar maxbytes_ endtime_ laststart_ srcid_ state_ u_ on_ranvar_
+    global ns opt src flowcdf
+
+    set laststart_ $starttime
+    if { $starttime <= [$ns now] } {
+        # starttime is lesser than current time, so it's high time we started.
+        set state_ ON
+        if { $opt(ontype) == "bytes" } {
+            set maxbytes_ [$on_ranvar_ value]; # in bytes
+        } elseif  { $opt(ontype) == "time" } {
+            set endtime_ [$on_ranvar_ value]; # in time
+        } else {
+            $u_ set min_ 0.0
+            $u_ set max_ 1.0
+            set r [$u_ value]
+            set idx [expr int(100000*$r)]
+            if { $idx > [llength $flowcdf] } {
+                set idx [expr [llength $flowcdf] - 1]
+            }
+            set maxbytes_ [expr 40 + [lindex $flowcdf $idx]]
+#            puts "Flow len $maxbytes_"
+        }
+        # puts "$starttime: Turning on $srcid_ for $maxbytes_ bytes $endtime_ sec"
+
+        $ns at [$ns now] "$src($srcid_) start"
+        # Start right away, because starttime was less than or equal to current time
+    } else {
+        # It's not yet time to start, so schedule an alarm to wake up just before $starttime
+        $self sched [expr $starttime - [$ns now]]
+        set state_ OFF
+    }
+}
+
+LoggingApp instproc timeout {} {
+    $self instvar srcid_ maxbytes_ endtime_
+    global ns src
+
+    # retry the go method now, it should be time to start
+    $self go [$ns now]
+}
+
+LoggingApp instproc recv { bytes } {
+    # there's one of these objects for each src/dest pair 
+    $self instvar nbytes_ srcid_ cumrtt_ numsamples_ maxbytes_ endtime_ laststart_ state_ u_ offtotal_ off_ranvar_ on_ranvar_ rtt_samples_
+    global ns opt src tp stats flowcdf
+
+    if { $state_ == OFF } {
+        if { [$ns now] >= $laststart_ } {
+#            puts "[$ns now]: wasoff turning $srcid_ on for $maxbytes_"
+            set state_ ON
+        }
+    }
+    
+    if { $state_ == ON } {
+        if { $bytes > 0 } {
+            set nbytes_ [expr $nbytes_ + $bytes]
+            set tcp_sender [lindex $tp($srcid_) 0]
+            set rtt_ [expr [$tcp_sender set rtt_] * [$tcp_sender set tcpTick_]]
+            if {$rtt_ > 0.0} {
+                set cumrtt_ [expr $rtt_  + $cumrtt_]
+                lappend rtt_samples_ $rtt_
+                set numsamples_ [expr $numsamples_ + 1]
+            }
+        }
+        set ontime [expr [$ns now] - $laststart_]
+        if { $nbytes_ >= $maxbytes_ || $ontime >= $endtime_ || $opt(simtime) <= [$ns now]} {
+#            puts "[$ns now]: Turning off $srcid_ ontime $ontime"
+            $ns at [$ns now] "$src($srcid_) stop"
+            $stats($srcid_) update $nbytes_ $ontime $cumrtt_ $numsamples_ $rtt_samples_
+            set nbytes_ 0
+            set state_ OFF
+            set nexttime [expr [$ns now] + [$off_ranvar_ value]]; # stay off until nexttime
+            set offtotal_ [expr $offtotal_ + $nexttime - [$ns now]]
+#            puts "OFFTOTAL for src $srcid_ $offtotal_"
+            set laststart_ $nexttime
+            if { $nexttime < $opt(simtime) } { 
+                # set up for next on period
+                if { $opt(ontype) == "bytes" } {
+                    set maxbytes_ [$on_ranvar_ value]; # in bytes
+                } elseif  { $opt(ontype) == "time" } {
+                    set endtime_ [$on_ranvar_ value]; # in time
+                } else {
+                    set r [$u_ value]
+                    set maxbytes_ [expr 40 + [ lindex $flowcdf [expr int(100000*$r)]]]
+                }
+                $ns at $nexttime: "$src($srcid_) start"; # schedule next start
+#                puts "@$nexttime: Turning on $srcid_ for $maxbytes_ bytes $endtime_ s"
+            }
+        }
+        return nbytes_
+    }
+}
+
+LoggingApp instproc results {} {
+    $self instvar nbytes_ cumrtt_ numsamples_ rtt_samples_
+    return [list $nbytes_ $cumrtt_ $numsamples_ $rtt_samples_]
+}
+
+LoggingApp instproc sample_off_duration {} {
+    $self instvar off_ranvar_
+    return [$off_ranvar_ value]
+}
---
+++ tcl/ex/congctrl/remyconf/dumbbell-buf1000-rtt150-bneck15.tcl
@@ -0,0 +1,56 @@
+# config file for remy simulations
+# this one is where all the sources are identical
+global opt
+
+# source, sink, and app types
+set opt(nsrc) 2;                # number of sources in experiment
+set opt(tcp) TCP/Reno
+set opt(sink) TCPSink
+set opt(cycle_protocols) false
+set protocols [list TCP/Newreno TCP/Vegas TCP/Vegas TCP/Newreno]; # don't put Linux TCPs first on list
+set protosinks [list TCPSink TCPSink TCPSink TCPSink]
+#set protocols [list TCP/Newreno TCP/Linux/cubic]
+#set protosinks [list TCPSink TCPSink/Sack1/DelAck]
+#set protocols [list TCP/Newreno TCP/Rational]
+set protocols [list TCP/Linux/cubic TCP/Linux/compound TCP/Linux/cubic TCP/Linux/compound ]
+set protosinks [list TCPSink/Sack1 TCPSink/Sack1 TCPSink/Sack1 TCPSink/Sack1 ]
+
+set opt(app) FTP
+set opt(pktsize) 1210
+set opt(rcvwin) 16384
+
+# topology parameters
+set opt(gw) DropTail;           # queueing at bottleneck
+set opt(bneck) 10Mb;             # bottleneck bandwidth (for some topos)
+set opt(maxq) 1000;             # max queue length at bottleneck
+set opt(delay) 49ms;            # total one-way delay in topology
+set opt(link) None
+
+# random on-off times for sources
+set opt(seed) 0
+set opt(onrand) Exponential
+set opt(offrand) Exponential
+set opt(onavg) 5.0;              # mean on and off time
+set opt(offavg) 5.0;              # mean on and off time
+set opt(avgbytes) 16000;          # 16 KBytes flows on avg (too low?)
+set opt(ontype) "time";           # valid options are "time" and "bytes"
+
+# simulator parameters
+set opt(simtime) 300.0;        # total simulated time
+set opt(tr) remyout;            # output trace in opt(tr).out
+set opt(partialresults) false;   # show partial throughput, delay, and utility scores?
+
+# utility and scoring
+set opt(alpha) 1.0
+set opt(tracewhisk) "none";     # give a connection ID to print for that flow, or give "all"
+
+proc set_access_params { nsrc } {
+    global accessdelay
+    for {set i 0} {$i < $nsrc} {incr i} {
+        set accessdelay($i) 1ms;       # latency of access link
+    }
+    global accessrate
+    for {set i 0} {$i < $nsrc} {incr i} {
+        set accessrate($i) 1000Mb;       # speed of access link
+    }
+}
