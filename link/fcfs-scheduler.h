9235c61c177b3057ec59ec8c4f3c9f6112f1b5ff
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-05-20 10:24:50 -0400

    Remove all occurences of est_foo_rate(now, nullptr) idiom

--- link/fcfs-scheduler.h
+++ link/fcfs-scheduler.h
@@ -27,7 +27,7 @@ class FcfsScheduler : public EnsembleScheduler {
   virtual uint32_t pick_user_to_schedule(void) const;
 
   /* Service rate of scheduler */
-  virtual double get_service_rate(uint32_t user_id) override { return flow_stats_.at(user_id).est_service_rate(Scheduler::instance().clock(), nullptr); }
+  virtual double get_service_rate(uint32_t user_id) override { return flow_stats_.at(user_id).ser_rate(); }
 
   /* Tcl interface : add links, and queues */
   virtual int command(int argc, const char*const* argv) override;
--- queue/sfd.h
+++ queue/sfd.h
@@ -48,7 +48,7 @@ class SFD : public EnsembleAwareQueue {
     virtual Packet* deque() override;
     virtual bool empty() const override { return (_packet_queue->byteLength() == 0); }
     virtual double get_hol() const override { return (empty()) ? DBL_MAX : hdr_cmn::access(_packet_queue->head())->timestamp(); }
-    virtual double get_arrival_rate() override { return _rate_estimator.est_arrival_rate(Scheduler::instance().clock(), nullptr); }
+    virtual double get_arrival_rate() override { return _rate_estimator.arr_rate(); }
     virtual int length() const override { return _packet_queue->length(); }
     virtual int byteLength() const override { return _packet_queue->byteLength(); }
     virtual Packet* get_head() const override { return _packet_queue->head(); }
