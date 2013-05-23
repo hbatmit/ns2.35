d83e01df1eb562b6ca9e7c56081625cda79c415b
Hari Balakrishnan <hari@csail.mit.edu>
2013-05-23 15:41:56 -0400

    Different RTT delays

--- tcl/ex/congctrl/remyconf/diffdelays.tcl
+++ tcl/ex/congctrl/remyconf/diffdelays.tcl
@@ -1,22 +1,30 @@
-# config file for remy simulations
-# this one is where all the sources are identical
+# Config file for remy simulations.
+# diffdelays.tcl: the connections all have different access delays and RTTs.
 global opt
 
 # source, sink, and app types
 set opt(nsrc) 2;                # number of sources in experiment
-set opt(tcp) TCP/Reno
+set opt(tcp) TCP/Rational
 set opt(sink) TCPSink
 set opt(cycle_protocols) false
-set opt(app) FTP
-set opt(pktsize) 1210
-set opt(rcvwin) 16384
+set protocols [list TCP/Newreno TCP/Linux/compound]; # don't put Linux TCPs first on list
+set protosinks [list TCPSink TCPSink]
+set protocols [list TCP/Newreno TCP/Linux/cubic]
+set protosinks [list TCPSink TCPSink/Sack1/DelAck]
+set protocols [list TCP/Newreno TCP/Rational]
+set protocols [list TCP/Newreno/Rational TCP/Linux/cubic ]
+set protosinks [list TCPSink/Sack1 TCPSink/Sack1/DelAck]
 
 # topology parameters
 set opt(gw) DropTail;           # queueing at bottleneck
-set opt(bneck) 10Mb;             # bottleneck bandwidth (for some topos)
+set opt(bneck) 1Mb;             # bottleneck bandwidth (for some topos)
 set opt(maxq) 1000;             # max queue length at bottleneck
 set opt(delay) 24ms;            # total one-way delay in topology
 set opt(link) None
+
+# app parameters
+set opt(app) FTP/OnOffSender
+set opt(pktsize) 1210;          # doesn't include proto headers
 
 # random on-off times for sources
 set opt(seed) 0
@@ -24,17 +32,20 @@ set opt(onrand) Exponential
 set opt(offrand) Exponential
 set opt(onavg) 5.0;              # mean on and off time
 set opt(offavg) 5.0;              # mean on and off time
-set opt(avgbytes) 16000;          # 16 KBytes flows on avg (too low?)
-set opt(ontype) "time";           # valid options are "time" and "bytes"
+set opt(avgbytes) 32000;          # 16 KBytes flows on avg (too low?)
+set opt(ontype) "bytes";           # valid options are "bytes" and "flowcdf"
 
 # simulator parameters
-set opt(simtime) 100.0;        # total simulated time
-set opt(tr) remyout;            # output trace in opt(tr).out
-set opt(partialresults) false;   # show partial throughput, delay, and utility scores?
+set opt(simtime) 300.0;        # total simulated time
+#set opt(tr) remyout;            # output trace in opt(tr).out
+set opt(partialresults) false;   # show partial throughput, delay, and utility?
+set opt(verbose) false;          # verbose printing for debugging (esp stats)
+set opt(checkinterval) 0.005;        # check stats every 5 ms
 
 # utility and scoring
 set opt(alpha) 1.0
 set opt(tracewhisk) "none";     # give a connection ID to print for that flow, or give "all"
+
 
 proc set_access_params { nsrc } {
     global accessdelay
@@ -46,4 +57,3 @@ proc set_access_params { nsrc } {
         set accessrate($i) 1000Mb;       # latency of access link
     }
 }
-
