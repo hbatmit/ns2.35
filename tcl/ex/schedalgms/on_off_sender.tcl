de328993e48f478509cf5e25b168e67c8d1566d9
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-05-26 10:52:59 -0400

    Break out FlowRanvar into its own class

--- tcl/ex/schedalgms/on_off_sender.tcl
+++ tcl/ex/schedalgms/on_off_sender.tcl
@@ -168,31 +168,3 @@ Application/FTP/OnOffSender instproc dumpstats {} {
         }
     }
 }
-
-Class FlowRanvar
-
-FlowRanvar instproc init {} {
-    global opt
-    $self instvar u_
-    set rng [new RNG]
-    set run [expr $opt(seed) + 2]
-    for { set j 1 } {$j < $run} {incr j} {
-        $rng next-substream
-    }
-    $self set u_ [new RandomVariable/Uniform]
-    $u_ set min_ 0.0
-    $u_ set max_ 1.0
-    $u_ use-rng $rng
-}
-
-FlowRanvar instproc value {} {
-    $self instvar u_
-    global flowcdf
-
-    set r [$u_ value]
-    set idx [expr int(100000 * $r)]
-    if { $idx > [llength $flowcdf] } {
-        set idx [expr [llength $flowcdf] - 1]
-    }
-    return  [expr 40 + [lindex $flowcdf $idx]]
-}
