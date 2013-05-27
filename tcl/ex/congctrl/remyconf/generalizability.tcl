4e2818a4f8fe2fe3a1cbad90c22b875d7d869bb7
Keith Winstein <keithw@ben.csail.mit.edu>
2013-05-27 18:58:32 -0400

    Add two rats

--- tcl/ex/congctrl/remyconf/generalizability.tcl
+++ tcl/ex/congctrl/remyconf/generalizability.tcl
@@ -37,7 +37,7 @@ set opt(ontype) "time";           # valid options are "bytes" and "flowcdf"
 set opt(reset) "true";              # reset TCP on end of ON period
 
 # simulator parameters
-set opt(simtime) 1000.0;        # total simulated time
+set opt(simtime) 10000.0;        # total simulated time
 #set opt(tr) remyout;            # output trace in opt(tr).out
 set opt(partialresults) false;   # show partial throughput, delay, and utility?
 set opt(verbose) false;          # verbose printing for debugging (esp stats)
