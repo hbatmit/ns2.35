02b0ca4f9bf23c1620f8b8adcf253a8aa0a0ec06
Keith Winstein <keithw@ben.csail.mit.edu>
2013-05-28 16:15:35 -0400

    Set up dumbbell config and import the delta=1 (timesten) whisker

--- tcl/ex/congctrl/remyconf/dumbbell-buf1000-rtt150-bneck15.tcl
+++ tcl/ex/congctrl/remyconf/dumbbell-buf1000-rtt150-bneck15.tcl
@@ -3,42 +3,45 @@
 global opt
 
 # source, sink, and app types
-set opt(nsrc) 2;                # number of sources in experiment
-set opt(tcp) TCP/Reno
+set opt(nsrc) 8;                # number of sources in experiment
+set opt(tcp) TCP/Rational
 set opt(sink) TCPSink
 set opt(cycle_protocols) false
-set protocols [list TCP/Newreno TCP/Vegas TCP/Vegas TCP/Newreno]; # don't put Linux TCPs first on list
+set protocols [list TCP/Rational TCP/Newreno TCP/Linux/cubic TCP/Linux/compound]; # don't put Linux TCPs first on list
 set protosinks [list TCPSink TCPSink TCPSink TCPSink]
 #set protocols [list TCP/Newreno TCP/Linux/cubic]
 #set protosinks [list TCPSink TCPSink/Sack1/DelAck]
 #set protocols [list TCP/Newreno TCP/Rational]
-set protocols [list TCP/Linux/cubic TCP/Linux/compound TCP/Linux/cubic TCP/Linux/compound ]
-set protosinks [list TCPSink/Sack1 TCPSink/Sack1 TCPSink/Sack1 TCPSink/Sack1 ]
-
-set opt(app) FTP
-set opt(pktsize) 1210
-set opt(rcvwin) 16384
+#set protocols [list TCP/Newreno/Rational TCP/Linux/cubic ]
+#set protosinks [list TCPSink/Sack1 TCPSink/Sack1/DelAck]
 
 # topology parameters
 set opt(gw) DropTail;           # queueing at bottleneck
-set opt(bneck) 10Mb;             # bottleneck bandwidth (for some topos)
+set opt(bneck) 15Mb;             # bottleneck bandwidth (for some topos)
 set opt(maxq) 1000;             # max queue length at bottleneck
-set opt(delay) 49ms;            # total one-way delay in topology
+set opt(delay) 74ms;            # total one-way delay in topology
 set opt(link) None
+
+# app parameters
+set opt(app) FTP/OnOffSender
+set opt(pktsize) 1210;          # doesn't include proto headers
 
 # random on-off times for sources
 set opt(seed) 0
 set opt(onrand) Exponential
 set opt(offrand) Exponential
 set opt(onavg) 5.0;              # mean on and off time
-set opt(offavg) 5.0;              # mean on and off time
-set opt(avgbytes) 16000;          # 16 KBytes flows on avg (too low?)
-set opt(ontype) "time";           # valid options are "time" and "bytes"
+set opt(offavg) 0.2;              # mean on and off time
+set opt(avgbytes) 32000;          # 16 KBytes flows on avg (too low?)
+set opt(ontype) "flowcdf";           # valid options are "bytes" and "flowcdf"
+set opt(reset) "false";             # reset TCP on end of ON period
 
 # simulator parameters
-set opt(simtime) 300.0;        # total simulated time
-set opt(tr) remyout;            # output trace in opt(tr).out
-set opt(partialresults) false;   # show partial throughput, delay, and utility scores?
+set opt(simtime) 100.0;        # total simulated time
+#set opt(tr) remyout;            # output trace in opt(tr).out
+set opt(partialresults) false;   # show partial throughput, delay, and utility?
+set opt(verbose) false;          # verbose printing for debugging (esp stats)
+set opt(checkinterval) 0.005;        # check stats every 5 ms
 
 # utility and scoring
 set opt(alpha) 1.0
