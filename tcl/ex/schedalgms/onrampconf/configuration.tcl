9f8dadf3bb7da4b7b52ed9af68a4bc140c5e2f04
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-05-29 07:25:07 -0400

    Bad bug, apparently Newreno doesn't respond to SACKs

--- tcl/ex/schedalgms/onrampconf/configuration.tcl
+++ tcl/ex/schedalgms/onrampconf/configuration.tcl
@@ -13,7 +13,7 @@ set opt(cdma_slot_duration) 0.00167
 set opt(ensemble_scheduler) pf
 set opt(cdma_ewma_slots)    100
 set opt(link_trace)         link.trace
-set opt(tcp)                TCP/Newreno
+set opt(tcp)                TCP/Sack1
 set opt(partialresults) false;    # show partial throughput, delay, and utility scores?
 set opt(alpha)              1.0;  # alpha for max-weight scheduling policy
 set opt(sub_qdisc)          propfair
