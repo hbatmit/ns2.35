e2ae6f0b78f6b06e25013a56778632f6fbfae95b
anirudhSK <sk.anirudh@gmail.com>
2013-06-09 12:42:34 -0400

    Modify QdelayEstimator to work with ns2 timestamps

--- queue/qdelay-estimator.cc
+++ queue/qdelay-estimator.cc
@@ -2,37 +2,55 @@
 #include <assert.h>
 #include <stdio.h>
 
-QdelayEstimator::QdelayEstimator( const std::queue<Packet> & pkts, uint64_t service_time) :
-	_pkts( pkts),
-	_service_time( service_time ),
-	_delays( std::vector<uint64_t> () )
+double QdelayEstimator::get_ts(Packet p) {
+	return hdr_cmn::access(&p)->timestamp();
+}
+
+double QdelayEstimator::get_seq(Packet p) {
+	return hdr_cmn::access(&p)->uid();
+}
+
+double QdelayEstimator::get_service_time(Packet p) {
+	return hdr_cmn::access(&p)->size()*8.0/_link_rate;
+}
+
+QdelayEstimator::QdelayEstimator(PacketQueue pkts, double link_rate) :
+	_pkts( std::queue<Packet>() ),
+	_link_rate(link_rate),
+	_delays(std::vector<double> ())
 {
+	Packet *p = pkts.getNext();
+	while (p != nullptr) {
+		_pkts.push(*(p->copy()));
+		p = pkts.getNext();
+	}
+	pkts.resetIterator();
 	assert( !_pkts.empty() );
 }
 
-std::vector<uint64_t> QdelayEstimator::estimate_delays( uint64_t current_tick )
+std::vector<double> QdelayEstimator::estimate_delays(double now)
 {
 	/* Base case(W_0) in Lindley's recurrence */
 	Packet hol_pkt = _pkts.front();
 	_pkts.pop();
-	int64_t current_delay = current_tick - hol_pkt._tick + _service_time;
+	double current_delay = now - get_ts(hol_pkt) + get_service_time(hol_pkt);
 	_delays.push_back( current_delay );
 	Packet previous_pkt = hol_pkt;
-	fprintf( stderr, "Lindley : seqnum %lu delay %ld\n", hol_pkt._seq_num, current_delay);
+	fprintf( stderr, "Lindley : seqnum %lu delay %ld\n", get_seq(hol_pkt), current_delay);
 
 	/* Apply Lindley's recurrence using constant service time */
 	while ( !_pkts.empty() ) {
 		Packet current_pkt = _pkts.front();
 		_pkts.pop();
-		int64_t inter_arrival_time = current_pkt._tick - previous_pkt._tick;
+		double inter_arrival_time = get_ts(current_pkt) - get_ts(previous_pkt);
+		double service_time = get_service_time(current_pkt);
 		assert( inter_arrival_time >= 0 );
-		current_delay = std::max( (int64_t) (current_delay + _service_time - inter_arrival_time), (int64_t) 0 );
+		current_delay = std::max( (int64_t) (current_delay + service_time - inter_arrival_time), (int64_t) 0 );
 		assert( current_delay >= 0 );
 		_delays.push_back( current_delay );
-		fprintf( stderr, "Lindley : seqnum %lu delay %ld\n", current_pkt._seq_num, current_delay);
+		fprintf( stderr, "Lindley : seqnum %lu delay %ld\n", get_seq(current_pkt), current_delay);
 		previous_pkt = current_pkt ;
 		}
-
 	/* return delays */
 	return _delays;
 }
--- queue/qdelay-estimator.hh
+++ queue/qdelay-estimator.hh
@@ -1,21 +1,26 @@
 #ifndef QDELAY_ESTIMATOR_HH
 #define QDELAY_ESTIMATOR_HH
 
-#include "../packet.hh"
 #include<queue>
+#include "common/packet.h"
+#include "queue/queue.h"
 
 /* Estimate future delays of packets in queue */
 class QdelayEstimator
 {
 private:
+	double get_ts(Packet p);
+        double get_seq(Packet p);
+        double get_service_time(Packet p);
+
 	std::queue<Packet> _pkts;
-	uint64_t _service_time;
-	std::vector<uint64_t> _delays;
+	double _link_rate;
+	std::vector<double> _delays;
 
 public:
-	QdelayEstimator( const std::queue<Packet> & pkts, uint64_t service_time );
+	QdelayEstimator(PacketQueue pkts, double link_rate);
 	
-	std::vector<uint64_t> estimate_delays( uint64_t current_tick );
+	std::vector<double> estimate_delays(double now);
 };
 
 #endif
