be2574cbbc3be4d4db6a0e1be0d90c086f408485
anirudhSK <sk.anirudh@gmail.com>
2013-06-22 18:35:03 -0400

    Make offtime a variable

--- tcl/ex/schedalgms/run-tcps.sh
+++ tcl/ex/schedalgms/run-tcps.sh
@@ -21,12 +21,13 @@ time_constant=`echo "scale = 3; 2 * $onewaydelay" | bc`
 time_constant=0.10
 thresh=`echo "scale = 3; $time_constant / 2.0" | bc`
 onbytes=10000000
+offtime=0.2
 set -x
 while [ $i -lt $iters ]; do
   echo $i
-  ../../../ns onramp.tcl -gw SFD      -nsrc 10 -simtime 100 -sched pf -link $link_trace -delay $onewaydelay -ontype bytes -avgbytes $onbytes -iter $i -offavg 0.2 -tcp $tcp_cc -cdma_slot 0.00167 -onramp_K $time_constant -droptype time -dth $thresh -percentile 50 2> /dev/null | grep sndrttMs > $outdir/onramp.$suffix-iter$i &
-  ../../../ns onramp.tcl -gw sfqCoDel -nsrc 10 -simtime 100 -sched pf -link $link_trace -delay $onewaydelay -ontype bytes -avgbytes $onbytes -iter $i -offavg 0.2 -tcp $tcp_cc -cdma_slot 0.00167 2> /dev/null | grep sndrttMs > $outdir/sfqCoDel.$suffix-iter$i &
-  ../../../ns onramp.tcl -gw DropTail -nsrc 10 -simtime 100 -sched pf -link $link_trace -delay $onewaydelay -ontype bytes -avgbytes $onbytes -iter $i -offavg 0.2  -tcp $tcp_cc -cdma_slot 0.00167 -maxq 1000000 2> /dev/null | grep sndrttMs > $outdir/DropTail.$suffix-iter$i &
+  ../../../ns onramp.tcl -gw SFD      -nsrc 10 -simtime 100 -sched pf -link $link_trace -delay $onewaydelay -ontype bytes -avgbytes $onbytes -iter $i -offavg $offtime -tcp $tcp_cc -cdma_slot 0.00167 -onramp_K $time_constant -droptype time -dth $thresh -percentile 50 2> /dev/null | grep sndrttMs > $outdir/onramp.$suffix-iter$i &
+  ../../../ns onramp.tcl -gw sfqCoDel -nsrc 10 -simtime 100 -sched pf -link $link_trace -delay $onewaydelay -ontype bytes -avgbytes $onbytes -iter $i -offavg $offtime -tcp $tcp_cc -cdma_slot 0.00167 2> /dev/null | grep sndrttMs > $outdir/sfqCoDel.$suffix-iter$i &
+  ../../../ns onramp.tcl -gw DropTail -nsrc 10 -simtime 100 -sched pf -link $link_trace -delay $onewaydelay -ontype bytes -avgbytes $onbytes -iter $i -offavg $offtime  -tcp $tcp_cc -cdma_slot 0.00167 -maxq 1000000 2> /dev/null | grep sndrttMs > $outdir/DropTail.$suffix-iter$i &
   i=`expr $i '+' 1`
 done
 set +x
