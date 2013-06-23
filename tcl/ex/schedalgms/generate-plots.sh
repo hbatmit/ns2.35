0a4a9470c0f64fe03721a4e1c883806bd6912cb5
anirudhSK <sk.anirudh@gmail.com>
2013-06-22 22:40:27 -0400

    Cosmetic changes to plot generation script

--- tcl/ex/schedalgms/generate-plots.sh
+++ tcl/ex/schedalgms/generate-plots.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-if [ $# -lt 1 ]; then
-  echo "Enter  rtt";
+if [ $# -lt 2 ]; then
+  echo "Enter  rtt and address";
   exit
 fi
 rtt=$1
+address=$2
 ./reduce.sh flowcdf.nconn10
 ../graphing-scripts/graphmaker flowcdf.nconn10 $rtt
-scp flowcdf.nconn10/graphdir/graph-10.svg anirudh@anirudh.csail.mit.edu:/tmp/result.svg
+scp flowcdf.nconn10/graphdir/graph-10.svg anirudh@$address:/tmp/result.svg
