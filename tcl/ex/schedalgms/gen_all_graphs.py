8d24d9636adcfe9b90f5e75fb7700f2db0ec55c1
anirudhSK <sk.anirudh@gmail.com>
2013-06-24 11:15:47 -0400

    Parametrize target_ip

--- tcl/ex/schedalgms/gen_all_graphs.py
+++ tcl/ex/schedalgms/gen_all_graphs.py
@@ -1,4 +1,6 @@
 #! /usr/bin/python
+import sys
+target_ip=sys.argv[1]
 from os import system
 for linkconfig in ["1mbps", "15mbps", "v3g", "v4g"]:
   for propdelay in [0.02, 0.075]:
@@ -9,6 +11,6 @@ for linkconfig in ["1mbps", "15mbps", "v3g", "v4g"]:
            linktrace="link-traces/"+str(nsrc)+"users"+linkconfig+".lt"
            suffix="link."+linktrace.split('/')[-1]+"."+str(propdelay)+"."+"nconn"+str(nsrc)+"."+"bytes"+"."+str(onbytes)+"."+cc.split('/')[-1]+"."+str(offtime);
 
-           genstr="./generate-plots.sh "+str(2*propdelay*1000.0)+" "+suffix+" "+"anirudh.csail.mit.edu"+" "+str(nsrc);
+           genstr="./generate-plots.sh "+str(2*propdelay*1000.0)+" "+suffix+" "+target_ip+" "+str(nsrc);
            print genstr
            system(genstr);
