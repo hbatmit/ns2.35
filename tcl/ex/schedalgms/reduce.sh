e4cebbea9a80e3991816ba92eefa4c51dc27e313
anirudhSK <sk.anirudh@gmail.com>
2013-06-25 12:52:27 -0400

    Check if you have the right number of entries before reducing

--- tcl/ex/schedalgms/reduce.sh
+++ tcl/ex/schedalgms/reduce.sh
@@ -1,8 +1,44 @@
 #! /bin/bash
+set -e
+if [ $# -lt 2 ]; then
+  echo "Enter suffix and nsrc";
+  exit 5;
+fi;
+
 suffix=$1
 outdir=$1
+nsrc=$2
 cd $outdir
+
+num_iters=`ls onramp.$suffix-iter* | wc -l`
+num_entries=`cat onramp.$suffix-iter* | wc -l`
+if [ $num_entries -ne `expr $num_iters '*' $nsrc` ];
+then
+  echo "Did not see the right number of entries, only saw $num_entries";
+  exit 5
+else
+  echo "Saw $num_entries, proceeding";
+fi;
 cat onramp.$suffix-iter*   > onramp.$suffix
+
+num_iters=`ls sfqCoDel.$suffix-iter* | wc -l`
+num_entries=`cat sfqCoDel.$suffix-iter* | wc -l`
+if [ $num_entries -ne `expr $num_iters '*' $nsrc` ]; then
+  echo "Did not see the right number of entries, only saw $num_entries";
+  exit 5
+else
+  echo "Saw $num_entries, proceeding";
+fi;
 cat sfqCoDel.$suffix-iter* > sfqCoDel.$suffix
+
+num_iters=`ls DropTail.$suffix-iter* | wc -l`
+num_entries=`cat DropTail.$suffix-iter* | wc -l`
+if [ $num_entries -ne `expr $num_iters '*' $nsrc` ]; then
+  echo "Did not see the right number of entries, only saw $num_entries";
+  exit 5
+else
+  echo "Saw $num_entries, proceeding";
+fi;
 cat DropTail.$suffix-iter* > DropTail.$suffix
+
 cd ..
