6a1a412bb76e663b748f891c4147b6e58a6f014f
anirudhSK <sk.anirudh@gmail.com>
2013-06-25 12:52:55 -0400

    Better error handling

--- tcl/ex/schedalgms/generate-plots.sh
+++ tcl/ex/schedalgms/generate-plots.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 if [ $# -lt 4 ]; then
   echo "Enter  rtt, suffix, address, and nsrc";
   exit
@@ -7,6 +8,6 @@ rtt=$1
 suffix=$2
 address=$3
 nsrc=$4
-./reduce.sh $suffix
+./reduce.sh $suffix $nsrc
 ../graphing-scripts/graphmaker $suffix $rtt
 scp $suffix/graphdir/graph-$nsrc.svg anirudh@$address:/tmp/result$suffix.svg
