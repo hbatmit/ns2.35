d274e770b60470fbcf17bd808ad6b8c0af8f1016
anirudhSK <sk.anirudh@gmail.com>
2013-07-09 12:10:24 -0400

    Print number of flows as well

--- tcl/ex/schedalgms/stats.tcl
+++ tcl/ex/schedalgms/stats.tcl
@@ -86,5 +86,5 @@ Stats instproc showstats { rcd_bytes rcd_avgrtt user_capacity tag} {
 
     set on_perc [expr 100.0*$ontime_ / $opt(simtime)]
 
-    puts [format "%s conn: %d rbytes: %d rMbps: %.3f fctMs: %.0f abytes: %d aMbps: %.3f sndrttMs %.1f rcdrttMs %.1f s_util: %.2f r_util: %.2f onperc: %.1f utilization: %.2f rtt95th %.1f capacity %.3f" $tag $srcid_ $rcd_bytes $rcdtput $fct $numbytes_ $throughput $avgrtt $rcd_avgrtt $util_s $util_r $on_perc [expr 100 * $throughput / $user_capacity] $rtt95th $user_capacity]
+    puts [format "%s conn: %d rbytes: %d rMbps: %.3f fctMs: %.0f abytes: %d aMbps: %.3f sndrttMs %.1f rcdrttMs %.1f s_util: %.2f r_util: %.2f onperc: %.1f utilization: %.2f rtt95th %.1f capacity %.3f num_flows %d" $tag $srcid_ $rcd_bytes $rcdtput $fct $numbytes_ $throughput $avgrtt $rcd_avgrtt $util_s $util_r $on_perc [expr 100 * $throughput / $user_capacity] $rtt95th $user_capacity $nflows_]
 }
