7ce89e408d1a35b85e77c2e24e498a1fd5d11877
anirudhSK <sk.anirudh@gmail.com>
2013-07-11 18:00:46 -0400

    Comput utilities in Python script instead of relying on ns2 output

--- tcl/ex/schedalgms/compute_stats.py
+++ tcl/ex/schedalgms/compute_stats.py
@@ -1,8 +1,10 @@
 #! /usr/bin/python
 import sys,re,numpy
+from math import log
 fh=sys.stdin
 reqd_tag=sys.argv[1]
 reqd_stat=sys.argv[2]
+prop_delay_ms=float(sys.argv[3])
 stats=[]
 for line in fh.readlines():
   line.strip();
@@ -16,13 +18,24 @@ for line in fh.readlines():
     if (reqd_stat == "fct"):
       stats += [float((re.search('(?<=fctMs: )\d+', line)).group(0))]
     elif (reqd_stat == "tpt"):
-      stats += [float((re.search('(?<=rMbps: )\d+\.\d+', line)).group(0))]
+      stats += [float((re.search('(?<=aMbps: )\d+\.\d+', line)).group(0))]
     elif (reqd_stat == "tail"):
       stats += [float((re.search('(?<=rtt95th )\d+\.\d+', line)).group(0))];
     elif (reqd_stat == "rtt"):
       stats += [float((re.search('(?<=sndrttMs )\d+\.\d+', line)).group(0))]
     elif (reqd_stat == "utility"):
-      stats += [float((re.search('(?<=s_util: )\d+\.\d+', line)).group(0))]
+      throughput  = float((re.search('(?<=aMbps: )\d+\.\d+', line)).group(0))
+      tail_delay  = float((re.search('(?<=rtt95th )\d+\.\d+', line)).group(0))
+      mean_delay  = float((re.search('(?<=sndrttMs )\d+\.\d+', line)).group(0))
+      fct         = float((re.search('(?<=fctMs: )\d+', line)).group(0))
+      if (tag == "web"):
+        util = log(1000000) - log(fct)
+      elif (tag == "bt"):
+        util = log(1000000) + log(throughput)
+      elif (tag == "stream"):
+        util = log(1000000) + log(throughput) - log(tail_delay - prop_delay_ms)
+      stats += [util]
+#      stats += [float((re.search('(?<=s_util: )\d+\.\d+', line)).group(0))]
     else :
       print "Invalid stat requested"
       exit(5)
