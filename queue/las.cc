c2181e330d5246e1abf75fc6eb2a9919785566b5
anirudhSK <sk.anirudh@gmail.com>
2013-08-27 15:15:02 -0400

    More precise LAS implementation

--- queue/las.cc
+++ queue/las.cc
@@ -30,9 +30,10 @@ void Las::enque(Packet *p) {
 
 Packet* Las::deque() {
   /* Implements pure virtual function Queue::deque() */
-  uint64_t chosen_flow = las();
 
+  uint64_t chosen_flow = las();
   if ( pkt_queues_.find(chosen_flow) != pkt_queues_.end() ) {
+    assert(pkt_queues_.at(chosen_flow)->length() > 0);
     Packet *p = pkt_queues_.at(chosen_flow)->deque();
     if (p != nullptr) {
       attained_service_.at(chosen_flow) += hdr_cmn::access(p)->size();
@@ -42,7 +43,7 @@ Packet* Las::deque() {
     }
     return p;
   } else {
-    return 0; /* empty */
+    return nullptr; /* empty */
   }
 }
 
@@ -62,10 +63,25 @@ uint64_t Las::hash(Packet* pkt) {
 }
 
 uint64_t Las::las() {
+  /* Clone attained_service_ map */
   typedef std::pair<uint64_t, uint32_t> FlowServices;
+  std::map<uint64_t,uint32_t> service_clone;
+
+  /* Purge all empty queues from consideration */
+  for (auto it = attained_service_.begin(); it != attained_service_.end(); it++) {
+    if (pkt_queues_.at(it->first)->length() > 0) {
+      service_clone[it->first] = attained_service_.at(it->first);
+    }
+  }
+
+  /* Among the non-empty queues, find the one with least sttained service */
   auto flow_compare = [&] (const FlowServices & q1, const FlowServices &q2 )
-                      { if (pkt_queues_.at(q1.first)->length() == 0) return false;
-                        else if (pkt_queues_.at(q2.first)->length() == 0) return true;
-                        else return q1.second < q2.second ; };
-  return std::min_element(attained_service_.begin(), attained_service_.end(), flow_compare) -> first;
+                      { assert (pkt_queues_.at(q1.first)->length() > 0);
+                        assert (pkt_queues_.at(q2.first)->length() > 0);
+                        return q1.second < q2.second ; };
+  if (service_clone.empty()) {
+    return -1;
+  } else {
+    return std::min_element(service_clone.begin(), service_clone.end(), flow_compare) -> first;
+  }
 }
