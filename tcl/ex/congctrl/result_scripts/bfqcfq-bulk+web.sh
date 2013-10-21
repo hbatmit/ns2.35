c3db1d10918fc411d26ae7ceb2eb53398e73f768
anirudhSK <sk.anirudh@gmail.com>
2013-10-21 17:17:22 -0400

    Change maxbins to 1024, add rttlog to prevent crashes

--- tcl/ex/congctrl/result_scripts/bfqcfq-bulk+web.sh
+++ tcl/ex/congctrl/result_scripts/bfqcfq-bulk+web.sh
@@ -11,8 +11,8 @@ seed=1
 
 while [ $seed -lt 75 ]; do
 
-  pls ./mixed_sims.tcl remyconf/vz4gdown.tcl -delay 0.150 -nsrc 2 -seed $seed -simtime 250 -reset false -verbose true -ontype flowcdf  -offavg 0.2 -gw sfqCoDel -tcp TCP/Newreno -sink TCPSink/Sack1 -maxq 100000000 -pktsize 1210 -rcvwin 10000000 -flowoffset 16384 -codel_target 1000.0 -maxbins 3  > 2flow$seed.droptail &
-  pls ./mixed_sims.tcl remyconf/vz4gdown.tcl -delay 0.150 -nsrc 2 -seed $seed -simtime 250 -reset false -verbose true -ontype flowcdf  -offavg 0.2 -gw sfqCoDel -tcp TCP/Newreno -sink TCPSink/Sack1 -maxq 100000000 -pktsize 1210 -rcvwin 10000000 -flowoffset 16384 -codel_target 0.005  -maxbins 3  > 2flow$seed.codel &
+  pls ./mixed_sims.tcl remyconf/vz4gdown.tcl -delay 0.150 -nsrc 2 -seed $seed -simtime 250 -reset false -verbose true -ontype flowcdf  -offavg 0.2 -gw sfqCoDel -tcp TCP/Newreno -sink TCPSink/Sack1 -maxq 100000000 -pktsize 1210 -rcvwin 10000000 -flowoffset 16384 -codel_target 1000.0 -maxbins 1024 -rttlog 2flow$seed.droptail.rtt > 2flow$seed.droptail &
+  pls ./mixed_sims.tcl remyconf/vz4gdown.tcl -delay 0.150 -nsrc 2 -seed $seed -simtime 250 -reset false -verbose true -ontype flowcdf  -offavg 0.2 -gw sfqCoDel -tcp TCP/Newreno -sink TCPSink/Sack1 -maxq 100000000 -pktsize 1210 -rcvwin 10000000 -flowoffset 16384 -codel_target 0.005  -maxbins 1024 -rttlog 2flow$seed.codel.rtt  > 2flow$seed.codel &
 
   seed=`expr $seed '+' 1`
 done
