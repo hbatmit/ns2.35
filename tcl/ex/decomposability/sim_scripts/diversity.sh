f1370c808b82a47dafb3a09db28dec5f3034edad
anirudhSK <sk.anirudh@gmail.com>
2014-01-21 18:57:49 -0500

    Diversity experiments, remember to increase queue size.

---
+++ tcl/ex/decomposability/sim_scripts/diversity.sh
@@ -0,0 +1,75 @@
+#! /bin/bash
+
+# Homogenous performance of independently evolved RemyCCs
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/delta-0.1.dna.5 ./decompose.tcl topotest.txt sd1.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/delta-0.1.1sender
+  run=`expr $run '+' 1`;
+done
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/delta-0.1.dna.5 ./decompose.tcl topotest.txt sd2.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/delta-0.1.2senders
+  run=`expr $run '+' 1`;
+done
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/delta-0.1.dna.5 ./decompose.tcl topotest.txt sd10.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/delta-0.1.10senders
+  run=`expr $run '+' 1`;
+done
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/delta-10.0.dna.5 ./decompose.tcl topotest.txt sd1.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/delta-10.0.1sender
+  run=`expr $run '+' 1`;
+done
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/delta-10.0.dna.5 ./decompose.tcl topotest.txt sd2.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/delta-10.0.2senders
+  run=`expr $run '+' 1`;
+done
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/delta-10.0.dna.5 ./decompose.tcl topotest.txt sd10.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/delta-10.0.10senders
+  run=`expr $run '+' 1`;
+done
+
+# Homogenous performance of coevolved RemyCCs
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/coexistencev2.dna.3.whiskertree1 ./decompose.tcl topotest.txt sd1.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/coevolved-delta-0.1.1sender
+  run=`expr $run '+' 1`;
+done
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/coexistencev2.dna.3.whiskertree1 ./decompose.tcl topotest.txt sd2.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/coevolved-delta-0.1.2senders
+  run=`expr $run '+' 1`;
+done
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/coexistencev2.dna.3.whiskertree1 ./decompose.tcl topotest.txt sd10.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/coevolved-delta-0.1.10senders
+  run=`expr $run '+' 1`;
+done
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/coexistencev2.dna.3.whiskertree2 ./decompose.tcl topotest.txt sd1.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/coevolved-delta-10.0.1sender
+  run=`expr $run '+' 1`;
+done
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/coexistencev2.dna.3.whiskertree2 ./decompose.tcl topotest.txt sd2.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/coevolved-delta-10.0.2senders
+  run=`expr $run '+' 1`;
+done
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=/tmp/coexistencev2.dna.3.whiskertree2 ./decompose.tcl topotest.txt sd10.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/coevolved-delta-10.0.10senders
+  run=`expr $run '+' 1`;
+done
