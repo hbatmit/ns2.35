2357c4da75c20dd61ef6a2ca5f68019b03698b89
anirudhSK <sk.anirudh@gmail.com>
2014-01-22 10:31:14 -0500

    qsize is 2 * BDP for these experiments. Remember to modify decompose.tcl to run against TCP/Newreno.

---
+++ tcl/ex/decomposability/sim_scripts/tcp_compatibility.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+set -x
+
+WHISKERS=ratsalone-baselineV2.dna.3 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 100000000000.0 -offrand Exponential -offavg 0.0 -simtime 100 -run 1 2>&1 | grep mbps > /tmp/baseline.alwayson
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=ratsalone-baselineV2.dna.3 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1 | grep mbps >> /tmp/baseline.1sec
+  run=`expr $run '+' 1`
+done
+
+WHISKERS=tcpantagonizeV2.dna.3 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 100000000000.0 -offrand Exponential -offavg 0.0 -simtime 100 -run 1 2>&1 | grep mbps > /tmp/antagonize.alwayson
+
+run=1;
+while [ $run -lt 11 ]; do
+  WHISKERS=tcpantagonizeV2.dna.3 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1 | grep mbps >> /tmp/antagonize.1sec
+  run=`expr $run '+' 1`
+done
