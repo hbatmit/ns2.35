cab3437382d61f9e4aad4d81696c44e5f9386424
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-01-28 21:02:47 -0500

    Increase number of runs to 50

--- tcl/ex/decomposability/sim_scripts/diversity.sh
+++ tcl/ex/decomposability/sim_scripts/diversity.sh
@@ -12,40 +12,40 @@ mkdir /tmp/coevolved-heterogenous
 set -x
 # Homogenous performance of independently evolved RemyCCs
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=delta-0.1.dna.5 ./decompose.tcl topotest.txt sd2.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/independent-homogenous/delta-0.1.2senders
   run=`expr $run '+' 1`;
 done
 
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=delta-10.0.dna.5 ./decompose.tcl topotest.txt sd2.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/independent-homogenous/delta-10.0.2senders
   run=`expr $run '+' 1`;
 done
 
 # Homogenous performance of coevolved RemyCCs
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=coexistencev2.dna.3.whiskertree1 ./decompose.tcl topotest.txt sd2.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/coevolved-homogenous/coevolved-delta-0.1.2senders
   run=`expr $run '+' 1`;
 done
 
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=coexistencev2.dna.3.whiskertree2 ./decompose.tcl topotest.txt sd2.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run 2>&1  | grep mbps >> /tmp/coevolved-homogenous/coevolved-delta-10.0.2senders
   run=`expr $run '+' 1`;
 done
 
 # Heterogenous performance of independently evolved RemyCCs
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=delta-0.1.dna.5 ./decompose.tcl topotest.txt sd2.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run -altwhisk delta-10.0.dna.5 2>&1  | grep mbps >> /tmp/independent-heterogenous/independent-2senders
   run=`expr $run '+' 1`;
 done
 
 # Heterogenous performance of coevolved RemyCCs
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=coexistencev2.dna.3.whiskertree1 ./decompose.tcl topotest.txt sd2.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 1.0 -offrand Exponential -offavg 1.0 -simtime 100 -run $run  -altwhisk coexistencev2.dna.3.whiskertree2 2>&1  | grep mbps >> /tmp/coevolved-heterogenous/coevolved-2senders
   run=`expr $run '+' 1`;
 done
