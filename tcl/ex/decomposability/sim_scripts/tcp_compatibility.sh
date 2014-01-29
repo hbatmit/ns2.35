5cc58308d16cfa7b125aad22cc9cb3593786629c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-01-28 20:56:04 -0500

    Increase number of runs to 50

--- tcl/ex/decomposability/sim_scripts/tcp_compatibility.sh
+++ tcl/ex/decomposability/sim_scripts/tcp_compatibility.sh
@@ -5,38 +5,38 @@ mkdir /tmp/withoutnewreno
 
 # Newreno alone
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Newreno -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 5.0 -offrand Exponential -offavg 0.01 -simtime 100 -run $run 2>&1 | grep mbps >> /tmp/withoutnewreno/newreno.alwayson
   run=`expr $run '+' 1`
 done
 
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Newreno -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 5.0 -offrand Exponential -offavg 5.0 -simtime 100 -run $run 2>&1 | grep mbps >> /tmp/withoutnewreno/newreno.5sec
   run=`expr $run '+' 1`
 done
 
 ###### Without Newreno #####
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=ratsonlybaselineV5-delta0.1.dna.2 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 5.0 -offrand Exponential -offavg 0.01 -simtime 100 -run $run 2>&1 | grep mbps >> /tmp/withoutnewreno/baseline.alwayson
   run=`expr $run '+' 1`
 done
 
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=ratsonlybaselineV5-delta0.1.dna.2 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 5.0 -offrand Exponential -offavg 5.0 -simtime 100 -run $run 2>&1 | grep mbps >> /tmp/withoutnewreno/baseline.5sec
   run=`expr $run '+' 1`
 done
 
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=tcpcompatibleV5-delta0.1.dna.3 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 5.0 -offrand Exponential -offavg 0.01 -simtime 100 -run $run 2>&1 | grep mbps >> /tmp/withoutnewreno/compatible.alwayson
   run=`expr $run '+' 1`
 done
 
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=tcpcompatibleV5-delta0.1.dna.3 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 5.0 -offrand Exponential -offavg 5.0 -simtime 100 -run $run 2>&1 | grep mbps >> /tmp/withoutnewreno/compatible.5sec
   run=`expr $run '+' 1`
 done
@@ -46,24 +46,24 @@ rm -rf /tmp/withnewreno
 mkdir /tmp/withnewreno
 
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=ratsonlybaselineV5-delta0.1.dna.2 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 5.0 -offrand Exponential -offavg 0.01 -simtime 100 -run $run -withnewreno 2>&1 | grep mbps >> /tmp/withnewreno/baseline.alwayson
   run=`expr $run '+' 1`
 done
 
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=ratsonlybaselineV5-delta0.1.dna.2 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 5.0 -offrand Exponential -offavg 5.0 -simtime 100 -run $run -withnewreno 2>&1 | grep mbps >> /tmp/withnewreno/baseline.5sec
   run=`expr $run '+' 1`
 done
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=tcpcompatibleV5-delta0.1.dna.3 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 5.0 -offrand Exponential -offavg 0.01 -simtime 100 -run $run -withnewreno 2>&1 | grep mbps >> /tmp/withnewreno/compatible.alwayson
   run=`expr $run '+' 1`
 done
 
 run=1;
-while [ $run -lt 11 ]; do
+while [ $run -lt 51 ]; do
   WHISKERS=tcpcompatibleV5-delta0.1.dna.3 ./decompose.tcl topocompat.txt sdcompat.txt -tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail -ontype time -onrand Exponential -onavg 5.0 -offrand Exponential -offavg 5.0 -simtime 100 -run $run -withnewreno 2>&1 | grep mbps >> /tmp/withnewreno/compatible.5sec
   run=`expr $run '+' 1`
 done
