bc39204a958a54a46f74658c01ba46ee7eb9f8c3
anirudhSK <sk.anirudh@gmail.com>
2014-05-22 18:18:30 -0400

    Optimality tests

--- tcl/ex/decomposability/sim_scripts/agilitysweep.py
+++ tcl/ex/decomposability/sim_scripts/agilitysweep.py
@@ -4,9 +4,9 @@ import numpy
 import math
 
 # constants
-iteration_count = 10
-resultfolder = "resultslogarithmic"
-topofolder = "topologarithmic"
+iteration_count = 50
+resultfolder = "resultsoptimal"
+topofolder = "topooptimal"
 
 # protocols
 rationalstr="-tcp TCP/Rational -sink TCPSink/Sack1 -gw DropTail"
@@ -23,17 +23,14 @@ os.system("rm -rf " + topofolder)
 os.system("mkdir " + topofolder)
 
 # Make delay toplogies
-for delay in range(5, 151, 5):
-  fh=open(topofolder + "/delay"+ str(delay) + ".txt", "w");
-  fh.write("0 1 " + str(10 ** 1.5) + " " + str(delay) + "\n");
-  fh.write("1 2 1000000000 1\n");
-  fh.write("1 3 1000000000 1\n");
-  fh.close();
+fh=open(topofolder + "/delay75.txt", "w");
+fh.write("0 1 " + str(10 ** 1.5) + " " + 75 + "\n");
+fh.close();
 
 # Make common sd file
 fh=open(topofolder + "/sd.txt", 'w');
-fh.write("0 2\n");
-fh.write("0 3\n");
+fh.write("0 1\n");
+fh.write("0 1\n");
 fh.close();
 
 # Synthesize command line
@@ -49,13 +46,11 @@ def synthesize( whiskertree, topology, tcp_agents, traffic_cfg, off_time, sim_ti
 synthesize.targets=""
 synthesize.cmdlines=""
 
-# Cross-agility on delay
-for delay in range(5, 151, 5):
-  delay_topology = topofolder + "/delay" + str(delay) + ".txt"
-  for run in range(1, iteration_count + 1):
-    synthesize( "/data/lsp/owenhsin/anirudh/prthaker/rtt-config-1/rtt_10x.dna.3",     delay_topology,     rationalstr,      traffic_workload, 1.0, 100, run, "rtt10x"+str(delay));
-    synthesize( "/data/lsp/owenhsin/anirudh/prthaker/rtt-config-2/rtt_20x.dna.4",     delay_topology,     rationalstr,      traffic_workload, 1.0, 100, run, "rtt20x"+str(delay));
-    synthesize( "/data/lsp/owenhsin/anirudh/prthaker/rtt-config-3/rtt_30x.dna.2",    delay_topology,     rationalstr,      traffic_workload, 1.0, 100, run, "rtt30x"+str(delay));
-    synthesize( "/data/lsp/owenhsin/anirudh/prthaker/rtt-and-link-speed/rtt_link.dna.3",    delay_topology,     rationalstr,      traffic_workload, 1.0, 100, run, "rttandlinkspeed"+str(delay));
+# Run all protocols on single configuration
+topology = topofolder + "/delay75.txt"
+for run in range(1, iteration_count + 1):
+  synthesize( "/home/anirudh/camera-ready/rats/rtt-150-alone.dna.5",     topology,     rationalstr,      traffic_workload, 1.0, 100, run, "remy");
+  synthesize( "NULL",                                                    topology,     cubicstr,         traffic_workload, 1.0, 100, run, "cubic");
+  synthesize( "NULL",                                                    topology,     cubicsfqCoDelstr, traffic_workload, 1.0, 100, run, "cubicsfqCoDel");
 
 print "all: " + synthesize.targets, synthesize.cmdlines
