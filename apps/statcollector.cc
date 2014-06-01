acedbf3af007ab22c722ceacfba85de2e3f00db6
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-01 19:48:37 -0400

    num_samples can sometimes be zero. Handle this.

--- apps/statcollector.cc
+++ apps/statcollector.cc
@@ -33,12 +33,11 @@ void StatCollector::add_sample(Packet* pkt) {
 void StatCollector::output_stats(double on_duration, uint32_t flow_id,
                                  uint32_t payload_size) {
   assert(on_duration != 0);
-  assert(num_samples_ != 0);
   fprintf(stderr,
           "%u: tp=%f mbps, del=%f ms, on=%f secs, samples=%d, inorder=%d\n",
           flow_id,
           (cumulative_pkts_ * 8.0 * payload_size)/ (1.0e6 * on_duration),
-          (cumulative_rtt_ * 1000.0) / num_samples_,
+          (cumulative_rtt_ * 1000.0) / ((num_samples == 0) ?  -1 : num_samples_),
           on_duration,
           num_samples_,
           cumulative_pkts_);
