8518585265803bba0f78c62eb28af930b9f1b6d1
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-04 21:21:12 -0400

    Add a default throughput and delay for analysis

--- tcl/ex/decomposability/sim_scripts/analysis.py
+++ tcl/ex/decomposability/sim_scripts/analysis.py
@@ -1,5 +1,5 @@
 # Analyse a particular results file
-def analyze(run, configuration, num_senders, requested_id):
+def analyze(run, configuration, num_senders, requested_id, def_tpt, def_del):
   tpt_delay = open(configuration + "run" + str( run ) +  ".err", "r");
   tpt = 0.0
   delay = 0.0
@@ -11,25 +11,31 @@ def analyze(run, configuration, num_senders, requested_id):
       if ((requested_id != "*")) : # haven't asked for all ids
         if (sender_id != requested_id): # ids don't match, continue
           continue
-      tpt += float( records[1].split('=')[1] )
-      delay += float( records[3].split('=')[1] )
+      current_tpt = float( records[1].split('=')[1] )
+      current_delay = float( records[3].split('=')[1] )
+      if (current_tpt == 0) :
+        current_tpt = def_tpt
+      if (current_delay == 0):
+        current_delay = def_del
+      tpt += current_tpt
+      delay += current_delay
       count += 1;
   assert(count == num_senders)
   return (tpt, delay, count)
 
-def analyze_helper(acc, y, configuration, num_senders, sender_id):
-  z = analyze(y, configuration, num_senders, sender_id);
+def analyze_helper(acc, y, configuration, num_senders, sender_id, def_tpt, def_del):
+  z = analyze(y, configuration, num_senders, sender_id, def_tpt, def_del);
   return (acc[0] + z[0], acc[1] + z[1], acc[2] + z[2]);
 
 # Aggregate results across runs on the same configuration
-def reduce_runs(run_range, configuration, num_senders, sender_id):
-  (sum_tpt, sum_delay, total_count) = reduce(lambda acc, y: analyze_helper(acc, y, configuration, num_senders, sender_id), run_range, (0.0, 0.0, 0));
+def reduce_runs(run_range, configuration, num_senders, sender_id, def_tpt, def_del):
+  (sum_tpt, sum_delay, total_count) = reduce(lambda acc, y: analyze_helper(acc, y, configuration, num_senders, sender_id, def_tpt, def_del), run_range, (0.0, 0.0, 0));
   assert(total_count == len(run_range) * num_senders);
   return (sum_tpt/total_count, sum_delay/total_count);
 
 # Plot aggregates into a plot file
-def plot_tpt_delay(run_range, filehandle, abscissa, num_senders, configuration, sender_id):
-  (tpt, delay) = reduce_runs(run_range, configuration, num_senders, sender_id);
+def plot_tpt_delay(run_range, filehandle, abscissa, num_senders, configuration, sender_id, def_tpt, def_del):
+  (tpt, delay) = reduce_runs(run_range, configuration, num_senders, sender_id, def_tpt, def_del);
   output_str = ""
   for x in abscissa:
     output_str += str(x) + " "
