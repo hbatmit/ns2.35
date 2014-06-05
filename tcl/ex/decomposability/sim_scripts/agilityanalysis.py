41efb9e98ffad76383a9614d54d4fd02c84292bf
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-05 13:06:55 -0400

    Modify analysis scripts to sweep rtts

--- tcl/ex/decomposability/sim_scripts/agilityanalysis.py
+++ tcl/ex/decomposability/sim_scripts/agilityanalysis.py
@@ -6,11 +6,14 @@ from analysis import *
 
 # Set constants
 iteration_count = 10
-resultfolder = "resultslogarithmic/"
+resultfolder = "results-rttvar/"
 
 # Cross-agility on delay
 for delay in range(5, 151, 5):
-  plot_tpt_delay(range(1, iteration_count + 1), open("plots/rtt10x.plot", "a"),          (delay, ), 2, resultfolder + "rtt10x"+str(delay), "*");
-  plot_tpt_delay(range(1, iteration_count + 1), open("plots/rtt20x.plot", "a"),          (delay, ), 2, resultfolder + "rtt20x"+str(delay), "*");
-  plot_tpt_delay(range(1, iteration_count + 1), open("plots/rtt30x.plot", "a"),          (delay, ), 2, resultfolder + "rtt30x"+str(delay), "*");
-  plot_tpt_delay(range(1, iteration_count + 1), open("plots/rttandlinkspeed.plot", "a"),          (delay, ), 2, resultfolder + "rttandlinkspeed"+str(delay), "*");
+  plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt150.plot", "a"),        (delay, ), 2, resultfolder + "rtt150"+str(delay), "*");
+  plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt140--160.plot", "a"),   (delay, ), 2, resultfolder + "rtt140--160"+str(delay), "*");
+  plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt110--200.plot", "a"),   (delay, ), 2, resultfolder + "rtt110--200"+str(delay), "*");
+  plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt50--250.plot", "a"),    (delay, ), 2, resultfolder + "rtt50--250"+str(delay), "*");
+  plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt10--280.plot", "a"),    (delay, ), 2, resultfolder + "rtt10--280"+str(delay), "*");
+  plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/cubic.plot", "a"),         (delay, ), 2, resultfolder + "cubic"+str(delay), "*");
+  plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/cubicsfqCoDel.plot", "a"), (delay, ), 2, resultfolder + "cubicsfqCoDel"+str(delay), "*");
