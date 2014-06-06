34ea6ee00f5763ea307289fe047c5aad40d8b550
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-06-05 23:10:29 -0400

    Modify scripts to reflect new DNA

--- tcl/ex/decomposability/sim_scripts/agilityanalysis.py
+++ tcl/ex/decomposability/sim_scripts/agilityanalysis.py
@@ -9,9 +9,10 @@ iteration_count = 10
 resultfolder = "results-rttvar/"
 
 # Cross-agility on delay
-for delay in range(5, 151, 5):
+for delay in range(1, 151, 1):
   plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt150.plot", "a"),        (delay, ), 2, resultfolder + "rtt150"+str(delay), "*");
   plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt140--160.plot", "a"),   (delay, ), 2, resultfolder + "rtt140--160"+str(delay), "*");
+  plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt145--155.plot", "a"),   (delay, ), 2, resultfolder + "rtt145--155"+str(delay), "*");
   plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt110--200.plot", "a"),   (delay, ), 2, resultfolder + "rtt110--200"+str(delay), "*");
   plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt50--250.plot", "a"),    (delay, ), 2, resultfolder + "rtt50--250"+str(delay), "*");
   plot_tpt_delay(range(1, iteration_count + 1), open("rttplots/rtt10--280.plot", "a"),    (delay, ), 2, resultfolder + "rtt10--280"+str(delay), "*");
