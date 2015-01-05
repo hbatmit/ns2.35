ae932292240b4b224d12d037d456cdd58440f9e2
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-01-05 11:42:27 -0800

    Add Keith's ezio class for more pedantic atoi/atod

---
+++ common/ezio.cc
@@ -0,0 +1,95 @@
+/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#include <unistd.h>
+#include "exception.hh"
+#include <cassert>
+
+#include "ezio.hh"
+
+using namespace std;
+
+/* blocking write of entire buffer */
+void writeall( const int fd, const string & buf )
+{
+    auto it = buf.begin();
+
+    while ( it != buf.end() ) {
+        it = write_some( fd, it, buf.end() );
+    }
+}
+
+string::const_iterator write_some( const int fd,
+                                   const string::const_iterator & begin,
+                                   const string::const_iterator & end )
+{
+    assert( end > begin );
+
+    ssize_t bytes_written = write( fd, &*begin, end - begin );
+
+    if ( bytes_written < 0 ) {
+        throw Exception( "write" );
+    } else if ( bytes_written == 0 ) {
+        throw Exception( "write returned 0" );
+    }
+
+    return begin + bytes_written;
+}
+
+string readall( const int fd, const size_t limit )
+{
+    char buffer[ ezio::read_chunk_size ];
+
+    assert( limit > 0 );
+
+    ssize_t bytes_read = read( fd, &buffer, min( ezio::read_chunk_size, limit ) );
+
+    if ( bytes_read == 0 ) {
+        /* end of file = client has closed their side of connection */
+        return string();
+    } else if ( bytes_read < 0 ) {
+        throw Exception( "read" );
+    } else {
+        /* successful read */
+        return string( buffer, bytes_read );
+    }
+}
+
+long int myatoi( const string & str, const int base )
+{
+    if ( str.empty() ) {
+        throw Exception( "myatoi", "Invalid integer string: empty" );
+    }
+
+    char *end;
+
+    errno = 0;
+    long int ret = strtol( str.c_str(), &end, base );
+
+    if ( errno != 0 ) {
+        throw Exception( "strtol" );
+    } else if ( end != str.c_str() + str.size() ) {
+        throw Exception( "myatoi", "Invalid decimal integer" + str );
+    }
+
+    return ret;
+}
+
+double myatod( const string & str )
+{
+    if ( str.empty() ) {
+        throw Exception( "myatod", "Invalid double string: empty" );
+    }
+
+    char *end;
+
+    errno = 0;
+    double ret = strtod( str.c_str(), &end );
+
+    if ( errno != 0 ) {
+        throw Exception( "strtod" );
+    } else if ( end != str.c_str() + str.size() ) {
+        throw Exception( "myatod", "Invalid double " + str );
+    }
+
+    return ret;
+}
---
+++ common/ezio.hh
@@ -0,0 +1,21 @@
+/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#ifndef EZIO_HH
+#define EZIO_HH
+
+#include <string>
+
+namespace ezio
+{
+    const size_t read_chunk_size = 1 << 18;
+}
+
+std::string readall( const int fd, const size_t limit = ezio::read_chunk_size );
+void writeall( const int fd, const std::string & buf );
+std::string::const_iterator write_some( const int fd,
+                                        const std::string::const_iterator & begin,
+                                        const std::string::const_iterator & end );
+long int myatoi( const std::string & str, const int base = 10 );
+double myatod( const std::string & str );
+
+#endif /* EZIO_HH */
