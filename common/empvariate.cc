b758551e5149da06608b01070984b4fe38df16c7
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-01-05 10:58:46 -0800

    Add utility class for empirical variates

---
+++ common/empvariate.cc
@@ -0,0 +1,21 @@
+#include <fstream>
+#include "empvariate.hh"
+EmpVariate::EmpVariate(std::string cdf_file, uint32_t t_run)
+    : run_(t_run),
+      rng_(RNG::defaultrng()) {
+  /* Read CDF file into flow array */
+  std::ifstream in_file(cdf_file);
+  uint32_t flow_size;
+  while (in_file >> flow_size) {
+    flow_array_.push_back(flow_size);
+  }
+
+  /* Reset until the appropriate run */
+  for (uint32_t i = 1; i <= run_; i++) {
+    rng_->reset_next_substream();
+  }
+}
+
+double EmpVariate::sample(void) {
+  return flow_array_.at(int(rng_->uniform(0, flow_array_.size() - 1)));
+}
---
+++ common/empvariate.hh
@@ -0,0 +1,25 @@
+#ifndef EMP_VARIATE_HH_
+#define EMP_VARIATE_HH_
+
+#include <string>
+#include <vector>
+#include <cstdint>
+#include "rng.h"
+
+/*
+   Empirical Random variate with a specific seed,
+   done the ns-2 way (Section 25.1.1. of
+   the manual)
+ */
+class EmpVariate {
+ public:
+  EmpVariate(std::string cdf_file, uint32_t t_run);
+  double sample(void);
+
+ private:
+  const uint32_t run_;
+  RNG* rng_;
+  std::vector<uint32_t> flow_array_;
+};
+
+#endif // EMP_VARIATE_HH_
