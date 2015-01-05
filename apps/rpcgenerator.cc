9163db1b9a28d0c035629caee2cae4ce37f7dc32
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-01-05 11:43:46 -0800

    Add flow arrival rate and flow size cdf file to RpcGenerator

--- apps/rpcgenerator.cc
+++ apps/rpcgenerator.cc
@@ -1,17 +1,34 @@
 #include <cassert>
 #include <string>
+#include "ezio.hh"
 #include "config.h"
 #include "rpcgenerator.hh"
 
-RpcGenerator::RpcGenerator()
-  : flow_size_dist_("", 1) {}
+RpcGenerator::RpcGenerator(const uint32_t & run,
+                           const double & arrival_rate,
+                           const std::string & cdf_file)
+    : flow_arrivals_(arrival_rate, run),
+      flow_size_dist_(cdf_file, run) {}
 
 static class RpcGeneratorClass : public TclClass {
  public:
   RpcGeneratorClass() : TclClass("RpcGenerator") {}
   TclObject* create(int argc, const char*const* argv) {
-    assert(argc == 4);
-    return new RpcGenerator();
+    assert(argc == 7);
+
+    /* Get arguments */
+    uint32_t run = myatoi(argv[4]);
+    assert(run > 0);
+
+    double flow_rate = myatod(argv[5]);
+    assert(flow_rate > 0.0);
+
+    std::string cdf_file = argv[6];
+    assert(cdf_file != "");
+
+    return new RpcGenerator(run,
+                            flow_rate,
+                            cdf_file);
   }
 } class_rpc_generator;
 
--- apps/rpcgenerator.hh
+++ apps/rpcgenerator.hh
@@ -4,16 +4,26 @@
 #include <vector>
 #include "tcp-full.h"
 #include "empvariate.hh"
+#include "expvariate.hh"
 
 class PoissonProcess {
-
-
+ public:
+  PoissonProcess(const double & event_rate,
+                 const uint32_t & run)
+    : inter_arrivals_(1.0 / event_rate, run) {};
+  double next_event_time(void) { return last_event_ += inter_arrivals_.sample(); }
+ private:
+  ExpVariate inter_arrivals_;
+  double last_event_ {0.0};
 };
 
 /* RPC Generator */
 class RpcGenerator : public TclObject {
  public:
-  RpcGenerator();
+  RpcGenerator(const uint32_t & run,
+               const double & arrival_rate,
+               const std::string & cdf_file);
+  RpcGenerator() = delete;
   int command(int argc, const char*const* argv) override;
  private:
   std::vector<std::pair<FullTcpAgent*, bool>> connection_pool_;
